b9c3881758e23821aa2c9b0cfc14693872a26c1d
rogan.dawes <rogan.dawes@69e6d614-4441-0410-9a8b-65af957f44db>
2008-05-05 16:10:20 +0000

    FIXME: Add a note for future work

--- trunk/src/org/owasp/esapi/interfaces/ISecurityConfiguration.java
+++ trunk/src/org/owasp/esapi/interfaces/ISecurityConfiguration.java
@@ -40,6 +40,11 @@ import org.owasp.esapi.Threshold;
  */
 public interface ISecurityConfiguration {
 
+	// FIXME: SecurityConfiguration should be hierarchical in nature,
+	// so that e.g. we can have a site wide file containing some input validation rules,
+	// and an application specific one that can override settings as necessary
+	// See java.util.Properties
+
 	/**
 	 * Gets the master password.
 	 * 
