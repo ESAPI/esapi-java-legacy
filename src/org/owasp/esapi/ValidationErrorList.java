06585c49b8846197ef70a1210aa7c0e42f918936
manico.james <manico.james@69e6d614-4441-0410-9a8b-65af957f44db>
2008-08-15 22:07:25 +0000

    added detailed recipe for validation inside of the javadoc

--- src/org/owasp/esapi/ValidationErrorList.java
+++ src/org/owasp/esapi/ValidationErrorList.java
@@ -32,6 +32,51 @@ import org.owasp.esapi.errors.ValidationException;
  * <img src="doc-files/Validator.jpg" height="600">
  * <P>
  * 
+ * To use the ValidationErrorList to execute groups of validation 
+ * attempts, your controller code would look something like:
+ * 
+ * <PRE>
+ * ValidationErrorList() errorList = new ValidationErrorList();.
+ * String name  = getValidInput("Name", form.getName(), "SomeESAPIRegExName1", 255, false, errorList);
+ * String address = getValidInput("Address", form.getAddress(), "SomeESAPIRegExName2", 255, false, errorList);
+ * Integer weight = getValidInteger("Weight", form.getWeight(), 1, 1000000000, false, errorList);
+ * Integer sortOrder = getValidInteger("Sort Order", form.getSortOrder(), -100000, +100000, false, errorList);
+ * request.setAttribute(errorList , "ERROR_LIST");
+ * </PRE>
+ * 
+ * The at your view layer you would be able to retrieve all
+ * of your error messages via a helper function like:
+ * 
+ * <PRE>
+ * public static ValidationErrorList getErrors() {          
+ *     HttpServletRequest request = ESAPI.httpUtilities().getCurrentRequest();
+ *     ValidationErrorList errors = new ValidationErrorList();
+ *     if (request.getAttribute(Constants.ERROR_LIST) != null) {
+ *        errors = (ValidationErrorList)request.getAttribute("ERROR_LIST");
+ *     }
+ * 	   return errors;
+ * }
+ * </PRE>
+ * 
+ * You can list all errors like:
+ * 
+ * <PRE>
+ * <%
+ *      for (Object vo : errorList.errors()) {
+ *         ValidationException ve = (ValidationException)vo;
+ * %>
+ * <%= ESAPI.encoder().encodeForHTML(ve.getMessage()) %><br/>
+ * <%
+ *     }
+ * %>
+ * </PRE>
+ * 
+ * And even check if a specific UI component is in error via calls like:
+ * 
+ * <PRE>
+ * errorList.getError("Name");
+ * </PRE>
+ * 
  * @author Jim Manico (jim.manico .at. aspectsecurity.com) 
  * 		   <a href="http://www.aspectsecurity.com">Aspect Security</a>
  * @since August 15, 2008
