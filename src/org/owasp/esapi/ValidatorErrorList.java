99951fe2e113e10c4c5e5b37942e0a234c776b23
manico.james <manico.james@69e6d614-4441-0410-9a8b-65af957f44db>
2008-08-15 18:58:39 +0000

    moving ValidatorErrorList to a concrete class

--- src/org/owasp/esapi/ValidatorErrorList.java
+++ src/org/owasp/esapi/ValidatorErrorList.java
@@ -11,63 +11,86 @@
  * LICENSE before you use, modify, and/or redistribute this software.
  * 
  * @author Jeff Williams <a href="http://www.aspectsecurity.com">Aspect Security</a>
+ * 
  * @created 2007
  */
 package org.owasp.esapi;
 
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.Hashtable;
 import java.util.List;
 
-import org.owasp.esapi.ValidatorErrorList;
 import org.owasp.esapi.errors.ValidationException;
 
-
 /**
- * The ValidatorErrorList interface defines a collection of errors
- * so that groups of validation functions can be called in
- * a non-blocking fashion.
+ * The ValidatorErrorList interface defines a collection of ValidationExceptions
+ * so that groups of validation functions can be called in a non-blocking fashion.
  * <P>
  * <img src="doc-files/Validator.jpg" height="600">
  * <P>
  * 
- * @author Jim Manico (jim.manico .at. aspectsecurity.com) <a
- *         href="http://www.aspectsecurity.com">Aspect Security</a>
+ * @author Jim Manico (jim.manico .at. aspectsecurity.com) 
+ * 		   <a href="http://www.aspectsecurity.com">Aspect Security</a>
  * @since August 15, 2008
  */
-public interface ValidatorErrorList {
+public class ValidatorErrorList {
 
 	/**
-	 * Returns true if this list of empty.
-	 * 
-	 * @return boolean
+	 * Error list of ValidationException's
 	 */
-    public boolean isEmpty();
+	private Hashtable errorList = new Hashtable();
+
+	/**
+	 * Adds a new error to list with a unique named context.
+	 * No action taken if either element is null. 
+	 * Existing contexts will be overwritten.
+	 * 
+	 * @param context unique named context for this ValidationErrorList
+	 * @param ve
+	 */
+	public void addError(String context, ValidationException ve) {
+		if ((context != null) && (ve != null)) {
+			errorList.put(context, ve);
+		}
+	}
 
 	/**
 	 * Returns list of ValidationException, or empty list of no errors exist.
 	 * 
 	 * @return List
 	 */
-    public List errors();
-    
+	public List errors() {
+		ArrayList validationExceptionList = new ArrayList(errorList.size());
+		for (Enumeration e = errorList.elements() ; e.hasMoreElements() ;) {
+			validationExceptionList.add((ValidationException)e.nextElement());
+	     }
+
+		return validationExceptionList;
+	}
+
 	/**
 	 * Retrieves error for given context if one exists.
 	 * 
 	 * @param context unique name for each error
 	 * @return ValidationException or null for given context
 	 */
-    public ValidationException getError(String context);
+	public ValidationException getError(String context) {
+		if (errorList == null || context == null) return null;
+		
+		Object returnValue = errorList.get(context);
+		if (returnValue == null) return null;
+		
+		return (ValidationException)returnValue;
+	}
 
 	/**
-	 * Adds a new error to list with a unique named context.
+	 * Returns true if this list of empty.
 	 * 
-	 * @param context unique named context for this ValidationErrorList
-	 * @param ve
+	 * @return boolean
 	 */
-    public void addError(String context, ValidationException ve);
-    
-	/**
-	 * Removes all errors from error list.
-	 */
-    public void clearErrors();
+	public boolean isEmpty() {
+		if ((errorList == null) || (errorList.size() == 0)) return true;
+		return false;
+	}
 }
-
