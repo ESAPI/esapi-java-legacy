f5816c3ef967b5a0c88344f538ea01e66cc00b47
planetlevel <planetlevel@69e6d614-4441-0410-9a8b-65af957f44db>
2008-12-27 03:55:56 +0000

    remove extraneous import

--- trunk/src/test/java/org/owasp/esapi/codecs/CodecTest.java
+++ trunk/src/test/java/org/owasp/esapi/codecs/CodecTest.java
@@ -15,8 +15,6 @@
  */
 package org.owasp.esapi.codecs;
 
-import org.owasp.esapi.reference.DefaultEncoder;
-
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
