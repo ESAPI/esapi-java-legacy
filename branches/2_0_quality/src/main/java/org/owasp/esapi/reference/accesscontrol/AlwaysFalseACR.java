bd22918d27fdb532500520b8795bca74836b05c1
manico.james <manico.james@69e6d614-4441-0410-9a8b-65af957f44db>
2009-07-21 00:01:15 +0000

    will migrate to a better name

--- branches/2_0_quality/src/main/java/org/owasp/esapi/reference/accesscontrol/AlwaysFalseACR.java
+++
@@ -1,9 +0,0 @@
-package org.owasp.esapi.reference.accesscontrol;
-
-public class AlwaysFalseACR extends BaseACR<Object, Object> {
-
-//	@Override
-	public boolean isAuthorized(Object runtimeParameter) {
-		return false;
-	}
-}
