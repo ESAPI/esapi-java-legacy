3f87b79c6bf4c3ca62ce8afc1a88037da9872313
chrisisbeef <chrisisbeef@69e6d614-4441-0410-9a8b-65af957f44db>
2009-07-22 05:56:23 +0000

    Adjusted reference implementations to extend the new AbstractAccessReferenceMap instead of directly implementing the AccessReferenceMap interface. Added Java5 generics.

--- src/main/java/org/owasp/esapi/reference/IntegerAccessReferenceMap.java
+++ src/main/java/org/owasp/esapi/reference/IntegerAccessReferenceMap.java
@@ -15,134 +15,59 @@
  */
 package org.owasp.esapi.reference;
 
-import java.util.HashMap;
-import java.util.Iterator;
 import java.util.Set;
-import java.util.TreeSet;
-
-import org.owasp.esapi.AccessReferenceMap;
-import org.owasp.esapi.errors.AccessControlException;
 
 /**
  * Reference implementation of the AccessReferenceMap interface. This
  * implementation generates integers for indirect references.
  * 
  * @author Jeff Williams (jeff.williams@aspectsecurity.com)
+ * @author Chris Schmidt (chrisisbeef@gmail.com)
  * @since June 1, 2007
  * @see org.owasp.esapi.AccessReferenceMap
  */
-public class IntegerAccessReferenceMap implements AccessReferenceMap {
+public class IntegerAccessReferenceMap extends AbstractAccessReferenceMap<String> {
 
 	private static final long serialVersionUID = 5311769278372489771L;
 
-	/** The itod (indirect to direct) */
-	HashMap itod = new HashMap();
-
-	/** The dtoi (direct to indirect) */
-	HashMap dtoi = new HashMap();
-
 	int count = 1;
-	
-	/**
-	 * This AccessReferenceMap implementation uses integers to
-	 * create a layer of indirection.
-	 */
-	public IntegerAccessReferenceMap() {
-		// call update to set up the references
-	}
 
-	/**
-	 * Instantiates a new access reference map with a set of direct references.
-	 * 
-	 * @param directReferences
-	 *            the direct references
+   /**
+	 * {@inheritDoc}
 	 */
-	public IntegerAccessReferenceMap(Set directReferences) {
-		update(directReferences);
-	}
+   public IntegerAccessReferenceMap()
+   {
+   }
 
-	/**
-	* {@inheritDoc}
-	*/
-	public Iterator iterator() {
-		TreeSet sorted = new TreeSet(dtoi.keySet());
-		return sorted.iterator();
-	}
-	
-	/**
-	* {@inheritDoc}
-	*/
-	public String addDirectReference(Object direct) {
-		if ( dtoi.keySet().contains( direct ) ) {
-			return (String)dtoi.get( direct );
-		}
-		String indirect = getUniqueReference();
-		itod.put(indirect, direct);
-		dtoi.put(direct, indirect);
-		return indirect;
-	}
-	
-	/**
-	 * Returns a reference guaranteed to be unique.
-	 * 
-	 * @return 
-	 * 		a unique reference 
+   /**
+	 * {@inheritDoc}
 	 */
-	private synchronized String getUniqueReference() {
+   public IntegerAccessReferenceMap(int initialSize)
+   {
+      super(initialSize);
+   }
+
+   /**
+	 * {@inheritDoc}
+	 */
+   public IntegerAccessReferenceMap(Set<Object> directReferences)
+   {
+      super(directReferences);
+   }
+
+   /**
+	 * {@inheritDoc}
+	 */
+   public IntegerAccessReferenceMap(Set<Object> directReferences, int initialSize)
+   {
+      super(directReferences, initialSize);
+   }
+
+   /**
+	 * {@inheritDoc}
+	 */
+	protected synchronized String getUniqueReference() {
 		return "" + count++;  // returns a string version of the counter
 	}
 	
-	/**
-	* {@inheritDoc}
-	*/
-	public String removeDirectReference(Object direct) throws AccessControlException {
-		String indirect = (String)dtoi.get(direct);
-		if ( indirect != null ) {
-			itod.remove(indirect);
-			dtoi.remove(direct);
-		}
-		return indirect;
-	}
-
-	/**
-	* {@inheritDoc}
-	*/
-	final public void update(Set directReferences) {
-		HashMap dtoi_old = (HashMap) dtoi.clone();
-		dtoi.clear();
-		itod.clear();
-
-		Iterator i = directReferences.iterator();
-		while (i.hasNext()) {
-			Object direct = i.next();
-
-			// get the old indirect reference
-			String indirect = (String) dtoi_old.get(direct);
-
-			// if the old reference is null, then create a new one that doesn't
-			// collide with any existing indirect references
-			if (indirect == null) {
-				indirect = getUniqueReference();
-			}
-			itod.put(indirect, direct);
-			dtoi.put(direct, indirect);
-		}
-	}
-
-	/**
-	* {@inheritDoc}
-	*/
-	public String getIndirectReference(Object directReference) {
-		return (String) dtoi.get(directReference);
-	}
-
-	/**
-	* {@inheritDoc}
-	*/
-	public Object getDirectReference(String indirectReference) throws AccessControlException {
-		if (itod.containsKey(indirectReference)) {
-			return itod.get(indirectReference);
-		}
-		throw new AccessControlException("Access denied", "Request for invalid indirect reference: " + indirectReference);
-	}
 }
