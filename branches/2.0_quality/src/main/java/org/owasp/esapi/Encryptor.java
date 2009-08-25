3134e76f7e8be118022f6e970b1f621a1d0fedeb
kevin.w.wall <kevin.w.wall@69e6d614-4441-0410-9a8b-65af957f44db>
2009-08-25 03:49:24 +0000

    Flushed out Javadoc. Added 2 new methods (encrypt() and decrypt() that take a SecretKey).

--- branches/2.0_quality/src/main/java/org/owasp/esapi/Encryptor.java
+++ branches/2.0_quality/src/main/java/org/owasp/esapi/Encryptor.java
@@ -15,6 +15,8 @@
  */
 package org.owasp.esapi;
 
+import javax.crypto.SecretKey;
+
 import org.owasp.esapi.errors.EncryptionException;
 import org.owasp.esapi.errors.IntegrityException;
 
@@ -90,6 +92,14 @@ public interface Encryptor {
 	
 	/**
 	 * Encrypts the provided plaintext and returns a ciphertext string.
+	 * </p><p>
+	 * This method uses the Electronic Code Book (ECB) cipher mode to encrypt,
+	 * which in the general case is not secure and usually should be avoided. It
+	 * is provided only for backward compatibility with ESAPI Java 1.4 code
+	 * and earlier. You should stop using this method an use the newer
+	 * encryption method (see deprecation warning) as soon as possible.
+	 * </p><p>
+	 * This method will likely be removed in some future ESAPI Java release.
 	 * 
 	 * @param plaintext
 	 *      the plaintext String to encrypt
@@ -100,20 +110,94 @@ public interface Encryptor {
 	 * @throws EncryptionException
 	 *      if the specified encryption algorithm could not be found or another problem exists with 
 	 *      the encryption of 'plaintext'
+	 * 
+	 * @see org.owasp.esapi.util.CryptoHelper#encrypt(String)
+	 * 
+	 * @deprecated As of ESAPI 2.0, replaced by {@link #encrypt(byte[])}
 	 */
-	String encrypt(String plaintext) throws EncryptionException;
+	@Deprecated String encrypt(String plaintext) throws EncryptionException;
 
 	/**
-	 * TODO
-	 * @param plaintext
-	 * @return
-	 * @throws EncryptionException
+	 * Encrypts the provided plaintext bytes using the cipher transformation
+	 * specified by the property <code>Encryptor.CipherTransformation</code>
+	 * and the <i>master encryption key</i> as specified by the property
+	 * {@code Encryptor.MasterKey} as defined in the <code>ESAPI.properties</code> file.
+	 * </p><p>
+	 * This method is preferred over {@link #encrypt(String)} because it also
+	 * allows encrypting of general byte streams rather than simply strings and
+	 * also because it returns a {@code CipherText} object and thus supports
+	 * cipher modes that require an Initialization Vector (IV), such as
+	 * Cipher Block Chaining (CBC).
+	 * 
+	 * @param plaintext	The byte stream to be encrypted. Note if a Java
+	 * 				{@code String} is to be encrypted, it should be converted
+	 * 				using {@code "some string".getBytes("UTF-8")}.
+	 * @return the {@code CipherText} object from which the raw ciphertext, the
+	 * 				IV, the cipher transformation, and many other aspects about
+	 * 				the encryption detail may be extracted.
+	 * @throws EncryptionException Thrown if something should go wrong such as
+	 * 				the JCE provider cannot be found, the cipher algorithm,
+	 * 				cipher mode, or padding scheme not being supported, specifying
+	 * 				an unsupported key size, specifying an IV of incorrect length,
+	 * 				etc.
+	 * @see #encrypt(SecretKey, byte[], boolean, boolean)
 	 */
 	 CipherText encrypt(byte[] plaintext) throws EncryptionException;
+
+
+	 /**
+	  * Encrypts the provided plaintext bytes using the cipher transformation
+	  * specified by the property <code>Encryptor.CipherTransformation</code>
+	  * as defined in the <code>ESAPI.properties</code> file and the
+	  * <i>specified secret key</i>.
+	  * </p><p>
+	  * This method is similar to {@link #encrypt(byte[])} except that it
+	  * permits a specific {@code SecretKey} to be used for encryption.
+	  *
+	  * @param key		The {@code SecretKey} to use for encrypting the plaintext.
+	  * @param plaintext	The byte stream to be encrypted. Note if a Java
+	  * 				{@code String} is to be encrypted, it should be converted
+	  * 				using {@code "some string".getBytes("UTF-8")}.
+	  * @param overwriteKey		If {@code true}, the {@code key} is overwritten
+	  * 				after the encryption is finished successfully. Only set
+	  * 				to true if the key has safely been saved elsewhere or by
+	  * 				the intended recipient of the encrypted message.
+	  * @param overwritePlaintext	f {@code true}, the {@code plaintext} is
+	  * 				overwritten after the encryption has finished successfully.
+	  * 				Only set this to true if you have no longer need the plaintext
+	  * 				after accessing it. The purpose of this flag is to help protect
+	  * 				against an adversary of gleaning sensitive data from memory or
+	  * 				a core dump.
+	  * @return the {@code CipherText} object from which the raw ciphertext, the
+	  * 				IV, the cipher transformation, and many other aspects about
+	  * 				the encryption detail may be extracted.
+	  * @throws EncryptionException Thrown if something should go wrong such as
+	  * 				the JCE provider cannot be found, the cipher algorithm,
+	  * 				cipher mode, or padding scheme not being supported, specifying
+	  * 				an unsupported key size, specifying an IV of incorrect length,
+	  * 				etc.
+	  * @see #encrypt(byte[])
+	  */
+	 CipherText encrypt(SecretKey key, byte[] plaintext, boolean overwriteKey, boolean overwritePlaintext)
+	 throws EncryptionException;
 
 	/**
 	 * Decrypts the provided ciphertext string (encrypted with the encrypt
 	 * method) and returns a plaintext string.
+	 * </p><p>
+	 * This is the decryption method that corresponds to the deprecated
+	 * {@link #encrypt(String)} method. It should only be used if you cannot
+	 * remove references to this deprecated encryption method. This method
+	 * expects ciphertext that was encrypted using the Electronic Code Book
+	 * (ECB) cipher mode which in the general case is not secure and should be
+	 * avoided.
+	 * </p><p>
+	 * This method is provided only for backward compatibility with ESAPI
+	 * Java 1.4 code and earlier. You should stop using these deprecated
+	 * encryption / decryption methods an use the newer encryption / 
+	 * decryption methods (see deprecation warning) as soon as possible.
+	 * </p><p>
+	 * This method will likely be removed in some future ESAPI Java release.
 	 * 
 	 * @param ciphertext
 	 *      the ciphertext (encrypted plaintext)
@@ -123,17 +207,63 @@ public interface Encryptor {
 	 * 
 	 * @throws EncryptionException
 	 *      if the specified encryption algorithm could not be found or another problem exists with 
-	 *      the encryption of 'plaintext'
+	 *      the decryption of 'plaintext'
+	 * 
+	 * @see org.owasp.esapi.util.CryptoHelper#decrypt(String)
+	 * 
+	 * @deprecated As of ESAPI 2.0, replaced by {@link #decrypt(CipherText)}s
 	 */
-	String decrypt(String ciphertext) throws EncryptionException;
+	@Deprecated String decrypt(String ciphertext) throws EncryptionException;
 
 	/**
-	 * TODO
-	 * @param ciphertext
-	 * @return
-	 * @throws EncryptionException
+	 * Decrypts the provided {@link CipherText} using the information from it
+	 * and the <i>master encryption key</i> as specified by the property
+	 * {@code Encryptor.MasterKey} as defined in the {@code ESAPI.properties}
+	 * file.
+	 * </p><p>
+	 * This decrypt method is to be preferred over the deprecated
+	 * {@link #decrypt(String)} method because this method can handle plaintext
+	 * bytes that were encrypted with cipher modes requiring IVs, such as CBC.
+	 * </p>
+	 * @param ciphertext The {@code CipherText} object to be decrypted.
+	 * @return The plaintext byte array resulting from decrypting the specified
+	 * 		   ciphertext. Note that it it is desired to convert the returned
+	 * 		   plaintext byte array to a Java String is should be done using
+	 * 		   {@code new String(byte[], "UTF-8");} rather than simply using
+	 * 		   {@code new String(byte[]);} which uses native encoding and may
+	 * 		   not be portable across hardware and/or OS platforms.
+	 * @throws EncryptionException  Thrown if something should go wrong such as
+	 * 				the JCE provider cannot be found, the cipher algorithm,
+	 * 				cipher mode, or padding scheme not being supported, specifying
+	 * 				an unsupported key size, or incorrect encryption key was
+	 * 				specified or a {@code PaddingException} occurs.
+	 * @see #decrypt(SecretKey, CipherText)
 	 */
 	byte[] decrypt(CipherText ciphertext) throws EncryptionException;
+	
+	/**
+	 * Decrypts the provided {@link CipherText} using the information from it
+	 * and the <i>specified secret key</i>.
+	 * </p><p>
+	 * This decrypt method is similar to {@link #decrypt(CipherText)} except that
+	 * it allows decrypting with a secret key other than the <i>master secret key</i>.
+	 * </p>
+	 * @param key		The {@code SecretKey} to use for encrypting the plaintext.
+	 * @param ciphertext The {@code CipherText} object to be decrypted.
+	 * @return The plaintext byte array resulting from decrypting the specified
+	 * 		   ciphertext. Note that it it is desired to convert the returned
+	 * 		   plaintext byte array to a Java String is should be done using
+	 * 		   {@code new String(byte[], "UTF-8");} rather than simply using
+	 * 		   {@code new String(byte[]);} which uses native encoding and may
+	 * 		   not be portable across hardware and/or OS platforms.
+	 * @throws EncryptionException  Thrown if something should go wrong such as
+	 * 				the JCE provider cannot be found, the cipher algorithm,
+	 * 				cipher mode, or padding scheme not being supported, specifying
+	 * 				an unsupported key size, or incorrect encryption key was
+	 * 				specified or a {@code PaddingException} occurs.
+	 * @see #decrypt(CipherText)
+	 */
+	byte[] decrypt(SecretKey key, CipherText ciphertext) throws EncryptionException;
 	
 	/**
 	 * Create a digital signature for the provided data and return it in a
