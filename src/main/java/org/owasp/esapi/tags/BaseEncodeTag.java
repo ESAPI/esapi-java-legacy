bb56391d78bee6039e60617a22a9f428f4bf265f
chrisisbeef <chrisisbeef@69e6d614-4441-0410-9a8b-65af957f44db>
2009-10-15 20:49:35 +0000

    Issue 35 - Refactor for ESAPI Tags
    Patch supplied by Ed Schaller
    
    http://code.google.com/p/owasp-esapi-java/issues/detail?id=35

---
+++ src/main/java/org/owasp/esapi/tags/BaseEncodeTag.java
@@ -0,0 +1,70 @@
+/*
+ * OWASP Enterprise Security API (ESAPI)
+ *
+ * This file is part of the Open Web Application Security Project (OWASP)
+ * Enterprise Security API (ESAPI) project. For details, please see
+ * <a href="http://www.owasp.org/index.php/ESAPI">http://www.owasp.org/index.php/ESAPI</a>.
+ *
+ * Copyright (c) 2007 - The OWASP Foundation
+ *
+ * The ESAPI is published by OWASP under the BSD license. You should read and accept the
+ * LICENSE before you use, modify, and/or redistribute this software.
+ *
+ * @author Jeff Williams <a href="http://www.aspectsecurity.com">Aspect Security</a>
+ * @created 2007
+ */
+
+package org.owasp.esapi.tags;
+
+import java.io.IOException;
+
+import javax.servlet.jsp.JspTagException;
+import javax.servlet.jsp.JspWriter;
+import javax.servlet.jsp.tagext.BodyTagSupport;
+
+import org.owasp.esapi.ESAPI;
+import org.owasp.esapi.Encoder;
+
+/** Abstract base class for tags that just encode their bodies with Encoder methods. */
+public abstract class BaseEncodeTag extends BodyTagSupport
+{
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * Encode tag's content.
+	 * @param content The tag's content as a String
+	 * @param enc Encoder provided as a convinence.
+	 * @return content encoded by the subclass's implementation.
+	 */
+	protected abstract String encode(String content, Encoder enc);
+
+	/**
+	 * After tag body parsing handler. This provides the necessary
+	 * plubming to allow subclasses to just concern themselves with
+	 * encoding a single string.
+	 * @return {@link javax.servlet.jsp.tagext.Tag#SKIP_BODY}
+	 * @throws JspTagException if writing to the bodyContent's
+	 * enclosing writer throws an IOException.
+	 */
+	public int doAfterBody() throws JspTagException
+	{
+		String content;
+		JspWriter out;
+
+		content = bodyContent.getString();
+		out = bodyContent.getEnclosingWriter();
+
+		content = encode(content, ESAPI.encoder());
+		try
+		{
+			out.print(content);
+		}
+		catch (IOException e)
+		{
+			throw new JspTagException("Error writing to body's enclosing JspWriter",e);
+		}
+
+		bodyContent.clearBody();
+		return SKIP_BODY;
+	}
+}
--- src/main/java/org/owasp/esapi/tags/EncodeForVBScriptTag.java
+++ src/main/java/org/owasp/esapi/tags/EncodeForVBScriptTag.java
@@ -1,51 +1,39 @@
+/*
+ * OWASP Enterprise Security API (ESAPI)
+ *
+ * This file is part of the Open Web Application Security Project (OWASP)
+ * Enterprise Security API (ESAPI) project. For details, please see
+ * <a href="http://www.owasp.org/index.php/ESAPI">http://www.owasp.org/index.php/ESAPI</a>.
+ *
+ * Copyright (c) 2007 - The OWASP Foundation
+ *
+ * The ESAPI is published by OWASP under the BSD license. You should read and accept the
+ * LICENSE before you use, modify, and/or redistribute this software.
+ *
+ * @author Jeff Williams <a href="http://www.aspectsecurity.com">Aspect Security</a>
+ * @created 2007
+ */
+
 package org.owasp.esapi.tags;
 
-import java.io.IOException;
-
-import javax.servlet.jsp.JspTagException;
-import javax.servlet.jsp.JspWriter;
-import javax.servlet.jsp.tagext.BodyContent;
-import javax.servlet.jsp.tagext.BodyTagSupport;
-
-import org.owasp.esapi.ESAPI;
 import org.owasp.esapi.Encoder;
 
 /**
- *
- * @author jwilliams
+ * JSP tag that encode's it's body for use in VBScript.
  */
-public class EncodeForVBScriptTag extends BodyTagSupport {
+public class EncodeForVBScriptTag extends BaseEncodeTag
+{
+	private static final long serialVersionUID = 3L;
 
 	/**
-	 * 
+	 * Encode tag's content for usage in VBScript.
+	 * @param content The tag's content as a String
+	 * @param enc Encoder used to call
+	 * 	{@link Encoder#encodeForVBScript}
+	 * @return content encoded for usage in VBScript
 	 */
-	private static final long serialVersionUID = 2L;
-	
-    /**
-     *
-     * @return
-     * @throws javax.servlet.jsp.JspTagException
-     */
-    public int doAfterBody() throws JspTagException {
-
-
-		BodyContent body = getBodyContent();
-		
-		String content = body.getString();
-		JspWriter out = body.getEnclosingWriter();
-		
-		Encoder e = ESAPI.encoder();
-		
-		try {
-			
-			out.print( e.encodeForVBScript(content) );
-			body.clearBody();
-			
-		} catch (IOException ioe) {
-			throw new JspTagException("error writing to body's enclosing writer",ioe);
-		}
-		
-		return SKIP_BODY;
-
+	protected String encode(String content, Encoder enc)
+	{
+		return enc.encodeForVBScript(content);
 	}
 }
