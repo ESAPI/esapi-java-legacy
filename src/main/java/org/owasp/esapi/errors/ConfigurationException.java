3ccc4a765aa903a6d86aeaa5581810d848fe1139
kevin.w.wall <kevin.w.wall@69e6d614-4441-0410-9a8b-65af957f44db>
2009-10-16 03:55:03 +0000

    Some Javadoc changes / improvements.

--- src/main/java/org/owasp/esapi/errors/ConfigurationException.java
+++ src/main/java/org/owasp/esapi/errors/ConfigurationException.java
@@ -1,15 +1,15 @@
 package org.owasp.esapi.errors;
 
 /**
- * A ConfigurationException should be thrown when a problem arises because of
+ * A {@code ConfigurationException} should be thrown when a problem arises because of
  * a problem in one of ESAPI's configuration files, such as a missing required
  * property or invalid setting of a property, or missing or unreadable
  * configuration file, etc.
  * <p>
- * A ConfigurationException is a RuntimeException because 1) configuration
- * properties can, for the most part, only be checked at run-time, and 2) we
- * want this to be an unchecked exception to make ESAPI easy to use and not
- * cluttered with catching a bunch of try/catch blocks.
+ * A {@code ConfigurationException} is a {@code RuntimeException}
+ * because 1) configuration properties can, for the most part, only be checked
+ * at run-time, and 2) we want this to be an unchecked exception to make ESAPI
+ * easy to use and not cluttered with catching a bunch of try/catch blocks.
  * </p>
  */
 public class ConfigurationException extends RuntimeException {
