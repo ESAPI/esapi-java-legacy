d101b17e0e4e53050fa7fc09e051a30f9330b4e6
kevin.w.wall <kevin.w.wall@69e6d614-4441-0410-9a8b-65af957f44db>
2009-10-20 04:36:28 +0000

    Minor changes to some Javadoc and other documentation. Minor change to overwrite(byte[], byte) method. Addition of new computeDerivedKey() method to compute derived key for encryption and authenticity. This method SHOULD be reviewed.

--- src/main/java/org/owasp/esapi/util/CryptoHelper.java
+++ src/main/java/org/owasp/esapi/util/CryptoHelper.java
@@ -9,24 +9,25 @@
  */
 package org.owasp.esapi.util;
 
-import java.io.IOException;
 import java.io.UnsupportedEncodingException;
+import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
 
 import javax.crypto.KeyGenerator;
+import javax.crypto.Mac;
 import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
 
 import org.owasp.esapi.ESAPI;
-import org.owasp.esapi.CipherText;
 import org.owasp.esapi.Logger;
 import org.owasp.esapi.errors.EncryptionException;
-import org.owasp.esapi.reference.DefaultCipherText;
 
 /**
  * Class to provide some convenience methods for encryption, decryption, etc.
  * </p><p>
  * All the cryptographic operations use the default cryptographic properties;
- * e.g., default cipher transformation, default key size, default IV type (if
+ * e.g., default cipher transformation, default key size, default IV type (where
  * applicable), etc.
  * 
  * @author kevin.w.wall@gmail.com
@@ -36,74 +37,8 @@ public class CryptoHelper {
 	
 	private static final Logger logger = ESAPI.getLogger("CryptoHelper");
 	
-	/**
-	 * Convenience method that encrypts plaintext strings the new way (default
-	 * is CBC mode and PKCS5 padding).
-	 * @param plaintext	A String to be encrypted
-	 * @return	A base64-encoded combination of IV + raw ciphertext
-	 * @throws EncryptionException	Thrown when something goes wrong with the
-	 * 								encryption.
-	 * 
-	 * @see org.owasp.esapi.Encryptor#encrypt(byte[])
-	 */
-	public static String encrypt(String plaintext) throws EncryptionException
-	{
-		CipherText ct = null;
-		try {
-			ct = ESAPI.encryptor().encrypt(plaintext.getBytes("UTF-8"));
-		} catch (UnsupportedEncodingException e) {
-			// Should never happen; UTF-8 should be in rt.jar.
-			logger.error(Logger.SECURITY_FAILURE, "UTF-8 encoding not available! Encryption failed.", e);
-			return null;	// CHECKME: Or re-throw or what? Could also use native encoding, but that's
-							// likely to cause unexpected and undesired effects far downstream.
-		}
-		return ct.getEncodedIVCipherText();
-	}
-
-	/**
-	 * Convenience method that decrypts previously encrypted plaintext strings
-	 * that were encrypted using the new encryption mechanism (with CBC mode and
-	 * PKCS5 padding by default).
-	 * @param b64IVCiphertext	A base64-encoded representation of the
-	 * 							IV + raw ciphertext string to be decrypted with
-	 * 							the default master key.
-	 * @return	The plaintext string prior to encryption.
-	 * @throws EncryptionException When something fails with the decryption.
-	 * 
-	 * @see org.owasp.esapi.Encryptor#decrypt(CipherText)
-	 */
-	public static String decrypt(String b64IVCiphertext) throws EncryptionException
-	{
-		DefaultCipherText ct = null;
-		try {
-			// We assume that the default cipher transform was used to encrypt this.
-			ct = new DefaultCipherText();
-			
-			// Need to base64 decode the IV+ciphertext and extract the IV to set it in DefaultCipherText object.
-			byte[] ivPlusRawCipherText = ESAPI.encoder().decodeFromBase64(b64IVCiphertext);
-			int blockSize = ct.getBlockSize();	// Size in bytes.
-			byte[] iv = new byte[ blockSize ];
-			copyByteArray(ivPlusRawCipherText, iv, blockSize);	// Copy the first blockSize bytes into iv array
-			int cipherTextSize = ivPlusRawCipherText.length - blockSize;
-			byte[] rawCipherText = new byte[ cipherTextSize ];
-			System.arraycopy(ivPlusRawCipherText, blockSize, rawCipherText, 0, cipherTextSize);
-			ct.setIVandCiphertext(iv, rawCipherText);
-			
-			// Now the DefaultCipherText object should be prepared to use it to decrypt.
-			byte[] plaintext = ESAPI.encryptor().decrypt(ct);
-			return new String(plaintext, "UTF-8");	// Convert back to a Java String using UTF-8 encoding
-		} catch (UnsupportedEncodingException e) {
-			// Should never happen; UTF-8 should be in rt.jar.
-			logger.error(Logger.SECURITY_FAILURE, "UTF-8 encoding not available! Decryption failed.", e);
-			return null;	// CHECKME: Or re-throw or what? Could also use native encoding, but that's
-							// likely to cause unexpected and undesired effects far downstream.
-		} catch (IOException e) {
-			logger.error(Logger.SECURITY_FAILURE, "Base64 decoding of IV+ciphertext failed. Decryption failed.", e);
-			e.printStackTrace(System.err);
-			return null;
-		}
-	}
-	
+	// TODO: Also consider supplying implementation of RFC 2898 / PKCS#5 PBKDF2
+	//		 in this file as well??? Maybe save for ESAPI 2.1 or 3.0.
 	/**
 	 * Generate a random secret key appropriate to the specified cipher algorithm
 	 * and key size.
@@ -133,7 +68,109 @@ public class CryptoHelper {
 					"Failed to generate secret key for " + alg + " with size of " + keySize + " bits.", e);
 		}
 	}
-	
+
+	// CHECKME: This method is critical and we can't afford to make changes to
+	//          it after ESAPI 2.0 is released or users with encrypted data
+	//		    using the new encryption / decryption may be forever screwed.
+	//			(See CAUTION, below, for details.) Strongly suggest that we
+	//			at least review this one class!!!
+	/**
+	 * Compute a derived key from the masterKey for either encryption / decryption
+	 * or for authentication.
+	 * <p>
+	 * <b>CAUTION:</b> If this algorithm for computing derived keys from master key
+	 * is <i>ever</i> changed, we risk breaking backward compatibility of being
+	 * able to decrypt data previously encrypted with earlier / different versions
+	 * of this method. Therefore, do not change this unless you know that
+	 * what you are doing will NOT change either of the derived keys for
+	 * ANY master key AT ALL!!!
+	 * 
+	 * @param masterKey		The "master" key from which the other keys are derived.
+	 * 						The derived key will have the same algorithm type as this
+	 * 						key.
+	 * @param keySize		The cipher's key size (in bits) for the {@code masterKey}.
+	 * 						Must have a minimum size of 56 bits and be an integral multiple of 8-bits.
+	 * 						The derived key will have the same size as this.
+	 * @param purpose		The purpose for the derived key. Must be either the
+	 * 						string "encryption" or "authenticity".
+	 * @return				The derived {@code SecretKey} to be used according
+	 * 						to the specified purpose.
+	 * @throws NoSuchAlgorithmException		The {@code masterKey} has an unsupported
+	 * 						encryption algorithm or no current JCE provider supports
+	 * 						"HmacSHA1".
+	 * @throws EncryptionException		If "UTF-8" is not supported as an encoding, then
+	 * 						this is thrown with the original {@code UnsupportedEncodingException}
+	 * 						as the cause. (NOTE: This should never happen as "UTF-8" is supposed to
+	 * 						be a common encoding supported by all Java implementations. Support
+	 * 					    for it is usually in rt.jar.)
+	 * @throws InvalidKeyException 	Likely indicates a coding error. Should not happen.
+	 * @throws EncryptionException 
+	 */
+	public static SecretKey computeDerivedKey(SecretKey masterKey, int keySize, String purpose)
+			throws NoSuchAlgorithmException, InvalidKeyException, EncryptionException
+	{
+		assert masterKey != null : "Master key cannot be null.";
+			// We would choose a larger minimum key size, but we want to be
+			// able to accept DES for legacy encryption needs.
+		assert keySize >= 56 : "Master key has size of " + keySize + ", which is less than minimum of 56-bits.";
+		assert (keySize % 8) == 0 : "Key size (" + keySize + ") must be a even multiple of 8-bits.";
+		assert purpose != null;
+		assert purpose.equals("encryption") || purpose.equals("authenticity") :
+			"Purpose must be \"encryption\" or \"authenticity\".";
+
+		keySize = (int) Math.ceil( keySize / 8 );	// Safely convert to whole # of bytes.
+		byte[] derivedKey = new byte[ keySize ];
+		byte[] tmpKey = null;
+		byte[] inputBytes;
+		try {
+			inputBytes = purpose.getBytes("UTF-8");
+		} catch (UnsupportedEncodingException e) {
+			throw new EncryptionException("Encryption failure (internal encoding error: UTF-8)",
+					 "UTF-8 encoding is NOT supported as a standard byte encoding: " + e.getMessage(), e);
+		}
+		
+			// Note that masterKey is going to be some SecretKey like an AES or
+			// DESede key, but not an HmacSHA1 key. That means it is not likely
+			// going to be 20 bytes but something different. Experiments show
+			// that doesn't really matter though as the SecretKeySpec CTOR on
+			// the following line still returns the appropriate sized key for
+			// HmacSHA1.
+		SecretKey sk = new SecretKeySpec(masterKey.getEncoded(), "HmacSHA1");
+		Mac mac = null;
+
+		try {
+			mac = Mac.getInstance("HmacSHA1");
+			mac.init(sk);
+		} catch( InvalidKeyException ex ) {
+			logger.error(Logger.SECURITY_FAILURE,
+					"Created HmacSHA1 Mac but SecretKey sk has alg " +
+					sk.getAlgorithm(), ex);
+			throw ex;
+		}
+
+		// Repeatedly call of HmacSHA1 hash until we've collected enough bits
+		// for the derived key. The first time through, we calculate the HmacSHA1
+		// on the "purpose" string, but subsequent calculations are performed
+		// on the previous result.
+		int totalCopied = 0;
+		int destPos = 0;
+		int len = 0;
+		do {
+			tmpKey = mac.doFinal(inputBytes);
+			if ( tmpKey.length >= keySize ) {
+				len = keySize;
+			} else {
+				len = Math.min(tmpKey.length, keySize - totalCopied);
+			}
+			System.arraycopy(tmpKey, 0, derivedKey, destPos, len);
+			inputBytes = tmpKey;
+			totalCopied += tmpKey.length;
+			destPos += len;
+		} while( totalCopied < keySize );
+		
+		return new SecretKeySpec(derivedKey, masterKey.getAlgorithm());
+	}
+
 	/**
 	 * Overwrite a byte array with a specified byte. This is frequently done
 	 * to a plaintext byte array so the sensitive data is not lying around
@@ -143,9 +180,7 @@ public class CryptoHelper {
 	 */
 	public static void overwrite(byte[] bytes, byte x)
 	{
-		for ( int i = 0; i < bytes.length; i++ ) {
-			bytes[i] = x;
-		}
+		Arrays.fill(bytes, x);
 	}
 	
 	/**
