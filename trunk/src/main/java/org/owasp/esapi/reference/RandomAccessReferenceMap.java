edaa2c4c3ea557ede129c74420b200897568b31e
chrisisbeef@gmail.com <chrisisbeef@gmail.com@69e6d614-4441-0410-9a8b-65af957f44db>
2009-10-27 21:18:35 +0000

    Removed extra no-arg ctor that showed up as a result of the merge from the 2.0_quality branch

--- trunk/src/main/java/org/owasp/esapi/reference/RandomAccessReferenceMap.java
+++ trunk/src/main/java/org/owasp/esapi/reference/RandomAccessReferenceMap.java
@@ -1,22 +1,22 @@
 /**
  * OWASP Enterprise Security API (ESAPI)
- * 
+ *
  * This file is part of the Open Web Application Security Project (OWASP)
  * Enterprise Security API (ESAPI) project. For details, please see
  * <a href="http://www.owasp.org/index.php/ESAPI">http://www.owasp.org/index.php/ESAPI</a>.
  *
  * Copyright (c) 2007 - The OWASP Foundation
- * 
+ *
  * The ESAPI is published by OWASP under the BSD license. You should read and accept the
  * LICENSE before you use, modify, and/or redistribute this software.
- * 
+ *
  * @author Jeff Williams <a href="http://www.aspectsecurity.com">Aspect Security</a>
  * @created 2007
  */
 package org.owasp.esapi.reference;
 
 import org.owasp.esapi.ESAPI;
-import org.owasp.esapi.Randomizer;
+import org.owasp.esapi.EncoderConstants;
 
 import java.util.Set;
 
@@ -27,47 +27,48 @@ import java.util.Set;
  * the random string approach provides a certain level of protection from CSRF
  * attacks, because an attacker would have difficulty guessing the indirect
  * reference.
- * 
+ *
  * @author Jeff Williams (jeff.williams@aspectsecurity.com)
  * @author Chris Schmidt (chrisisbeef@gmail.com)
- * @since June 1, 2007
  * @see org.owasp.esapi.AccessReferenceMap
+ * @since June 1, 2007
  */
-public class RandomAccessReferenceMap extends AbstractAccessReferenceMap<String> {
-	
-	private static final long serialVersionUID = 8544133840739803001L;
+public class RandomAccessReferenceMap extends AbstractAccessReferenceMap<String>
+{
 
-   public RandomAccessReferenceMap()
-   {
-   }
+   private static final long serialVersionUID = 8544133840739803001L;
 
    public RandomAccessReferenceMap(int initialSize)
    {
       super(initialSize);
    }
 
-	/**
-	 * This AccessReferenceMap implementation uses short random strings to
-	 * create a layer of indirection. Other possible implementations would use
-	 * simple integers as indirect references.
-	 */
-	public RandomAccessReferenceMap() {
-		// call update to set up the references
-	}
+   /**
+    * This AccessReferenceMap implementation uses short random strings to
+    * create a layer of indirection. Other possible implementations would use
+    * simple integers as indirect references.
+    */
+   public RandomAccessReferenceMap()
+   {
+      // call update to set up the references
+   }
 
    public RandomAccessReferenceMap(Set<Object> directReferences, int initialSize)
    {
       super(directReferences, initialSize);
-	}
+   }
 
-	/**
-	* {@inheritDoc}
-	*/
-	protected synchronized String getUniqueReference() {
-		String candidate;
-		do {
-			candidate = ESAPI.randomizer().getRandomString(6, DefaultEncoder.CHAR_ALPHANUMERICS);
-		} while (itod.keySet().contains(candidate));
-		return candidate;
-	}
+   /**
+    * {@inheritDoc}
+    */
+   protected synchronized String getUniqueReference()
+   {
+      String candidate;
+      do
+      {
+         candidate = ESAPI.randomizer().getRandomString(6, EncoderConstants.CHAR_ALPHANUMERICS);
+      }
+      while (itod.keySet().contains(candidate));
+      return candidate;
+   }
 }
