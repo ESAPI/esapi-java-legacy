c925752aad7d345d2300df4056eddcbfa0147191
planetlevel <planetlevel@69e6d614-4441-0410-9a8b-65af957f44db>
2009-11-04 06:00:42 +0000

    Initial release

--- trunk/src/main/java/org/owasp/esapi/PreparedString.java
+++ trunk/src/main/java/org/owasp/esapi/PreparedString.java
@@ -11,42 +11,175 @@
  * LICENSE before you use, modify, and/or redistribute this software.
  * 
  * @author Jeff Williams <a href="http://www.aspectsecurity.com">Aspect Security</a>
- * @created 2007
+ * @created 2009
  */
 package org.owasp.esapi;
 
+import java.util.ArrayList;
+import org.owasp.esapi.codecs.Codec;
+import org.owasp.esapi.codecs.HTMLEntityCodec;
+
 
 /**
- * A parameterized string that can be used to send data to an interpreter.
+ * A parameterized string that uses escaping to make untrusted data safe before combining it with
+ * a command or query intended for use in an interpreter.
  * <pre> 
- * PreparedString div = new PreparedString( "&lt;a href=\"@1\" onmouseover=\"alert('@2')\"&gt;test&lt;/a&gt;" );
- * div.setURL( 1, request.getParameter( "url" ) );
- * div.setJavaScriptString( 2, request.getParameter( "message" ) );
+ * PreparedString div = new PreparedString( "&lt;a href=\"http:\\\\example.com?id=?\" onmouseover=\"alert('?')\"&gt;test&lt;/a&gt;", new HTMLEntityCodec() );
+ * div.setURL( 1, request.getParameter( "url" ), new PercentCodec() );
+ * div.set( 2, request.getParameter( "message" ), new JavaScriptCodec() );
  * out.println( div.toString() );
  * 
  * // escaping for SQL
- * PreparedString query = new PreparedString( "SELECT * FROM users WHERE name=@1 AND password=@2" );
- * query.setSQLString( 1, request.getParameter( "name" ) );
- * query.setSQLString( 1, request.getParameter( "pass" ) );
+ * PreparedString query = new PreparedString( "SELECT * FROM users WHERE name=? AND password=?", new OracleCodec() );
+ * query.set( 1, request.getParameter( "name" ) );
+ * query.set( 2, request.getParameter( "pass" ) );
  * stmt.execute( query.toString() );
  * </pre>
  * 
- * @author Jeff Williams (jeff.williams .at. aspectsecurity.com) <a
- *         href="http://www.aspectsecurity.com">Aspect Security</a>
+ * @author Jeff Williams (jeff.williams .at. aspectsecurity.com) <a href="http://www.aspectsecurity.com">Aspect Security</a>
  * @since June 1, 2007
  */
 public class PreparedString {
-	String template = null;
-	char parameterCharacter = '@';
-	
-	public PreparedString( String template ) {
-		this.template = template;
+	char parameterCharacter = '?';
+	Codec codec = null;
+	String[] parameters = null;
+	ArrayList parts = new ArrayList();
+	private final static char[] IMMUNE = {};
+
+	/**
+	 * Create a PreparedString with the supplied template and Codec. The template should use the 
+	 * default parameter placeholder character (?) in the place where actual parameters are to be inserted.
+	 * The supplied Codec will be used to escape characters in calls to set, unless a specific Codec is
+	 * provided to override it.
+	 * @param template
+	 * @param codec
+	 */
+	public PreparedString( String template, Codec codec ) {
+		this.codec = codec;
+		split( template, parameterCharacter );
+	}
+
+	/**
+	 * Create a PreparedString with the supplied template, parameter placeholder character, and Codec. The parameter character
+	 * can be any character, but should not be one that will be used in the template. The parameter character can safely
+	 * be used in a parameter passed into the set methods.
+	 * @param template
+	 * @param parameterCharacter
+	 * @param codec
+	 */
+	public PreparedString( String template, char parameterCharacter, Codec codec ) {
+		this.codec = codec;
+		this.parameterCharacter = parameterCharacter;
+		split( template, parameterCharacter );
+	}
+
+	/**
+	 * Split a string with a particular character.
+	 * @param str
+	 * @param c
+	 */
+	private void split( String str, char c ) {
+		int index = 0;
+		int pcount = 0;
+		for ( int i = 0; i < str.length(); i++ ) {
+			if ( str.charAt(i) == c ) {
+				pcount++;
+				parts.add( str.substring(index,i) );
+				index = i + 1;
+			}
+		}
+		parts.add( str.substring(index) );
+		parameters = new String[pcount];
 	}
 	
-	public void setParameterCharacter( char c ) {
-		parameterCharacter = c;
+	/**
+	 * Set the parameter at index with supplied value using the default Codec to escape. 
+	 * @param index
+	 * @param value
+	 */
+	public void set( int index, String value ) {
+		if ( index < 1 || index > parameters.length ) {
+			throw new IllegalArgumentException( "Attempt to set parameter " + index + " on a PreparedString with only " + parameters.length + " placeholders" );
+		}
+		String encoded = codec.encode( IMMUNE, value );
+		parameters[index-1] = encoded;
 	}
 	
-	/// FIXME: xxx
+	/**
+	 * Set the parameter at index with supplied value using the supplied Codec to escape. 
+	 * @param index
+	 * @param value
+	 * @param codec
+	 */
+	public void set( int index, String value, Codec codec ) {
+		if ( index < 1 || index > parameters.length ) {
+			throw new IllegalArgumentException( "Attempt to set parameter " + index + " on a PreparedString with only " + parameters.length + " placeholders" );
+		}
+		String encoded = codec.encode( IMMUNE, value );
+		parameters[index-1] = encoded;
+	}
+	
+	/**
+	 * Render the PreparedString by combining the template with properly escaped parameters.
+	 */
+	public String toString() {
+		for ( int ix = 0; ix < parameters.length; ix++ ) {
+			if ( parameters[ix] == null ) {
+				throw new RuntimeException( "Attempt to render PreparedString without setting parameter " + ( ix + 1 ));
+			}
+		}
+		StringBuilder sb = new StringBuilder();
+		int i = 0;
+		for ( int p=0; p < parts.size(); p++ ) {
+			sb.append( parts.get( p ) );
+			if ( i < parameters.length ) sb.append( parameters[i++] );
+		}
+		return sb.toString();
+	}
+	
+	
+	public static void main( String[] args ) {
+		PreparedString ps1 = new PreparedString( "Test ? is ?", new HTMLEntityCodec() );
+		ps1.set( 1, "[]<>;\"\'PreparedString" );
+		ps1.set( 2, "cool" );
+		System.out.println( ps1 );
+		try {
+			PreparedString ps2 = new PreparedString( "Test ? is ?", new HTMLEntityCodec() );
+			ps2.set( 2, "cool" );
+			System.out.println( ps2 );
+		} catch( Exception e ) {
+			e.printStackTrace();
+		}
+
+		try {
+			PreparedString ps3 = new PreparedString( "Test ? is ?", new HTMLEntityCodec() );
+			ps3.set( 1, "[]<>;\"\'PreparedString" );
+			ps3.set( 2, "cool" );
+			ps3.set( 3, "cool" );
+			System.out.println( ps3 );
+		} catch( Exception e ) {
+			e.printStackTrace();
+		}
+
+		try {
+			PreparedString ps4 = new PreparedString( "???", new HTMLEntityCodec() );
+			ps4.set( 1, "1" );
+			ps4.set( 2, "2" );
+			ps4.set( 3, "3" );
+			System.out.println( ps4 );
+		} catch( Exception e ) {
+			e.printStackTrace();
+		}
+
+		try {
+			PreparedString ps5 = new PreparedString( "??x", new HTMLEntityCodec() );
+			ps5.set( 1, "1" );
+			ps5.set( 2, "2" );
+			System.out.println( ps5 );
+		} catch( Exception e ) {
+			e.printStackTrace();
+		}
+
+	}
 	
 }
