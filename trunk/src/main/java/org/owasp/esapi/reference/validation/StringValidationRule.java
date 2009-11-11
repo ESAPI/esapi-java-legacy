d7417ad4facf3589b48eaa261fe04b51add44c53
schallee@darkmist.net <schallee@darkmist.net@69e6d614-4441-0410-9a8b-65af957f44db>
2009-11-11 23:08:36 +0000

    Validate the string both before and after canonicalization. Previously it was only validated after. This also adds a setValidateInputAndCanonical(boolean) method that, when passed false, will make StringValidationRule behave as before.
    To simplify this a fair bit was refactored as well.
    
    This closes issue 54.

--- trunk/src/main/java/org/owasp/esapi/reference/validation/StringValidationRule.java
+++ trunk/src/main/java/org/owasp/esapi/reference/validation/StringValidationRule.java
@@ -25,6 +25,7 @@ import org.owasp.esapi.EncoderConstants;
 import org.owasp.esapi.errors.EncodingException;
 import org.owasp.esapi.errors.ValidationException;
 import org.owasp.esapi.util.StringUtils;
+import org.owasp.esapi.util.NullSafe;
 
 
 /**
@@ -44,11 +45,12 @@ public class StringValidationRule extends BaseValidationRule {
 	protected List<Pattern> blacklistPatterns = new ArrayList<Pattern>();
 	protected int minLength = 0;
 	protected int maxLength = Integer.MAX_VALUE;
+	protected boolean validateInputAndCanonical = true;
 
 	public StringValidationRule( String typeName ) {
 		super( typeName );
 	}
-	
+
 	public StringValidationRule( String typeName, Encoder encoder ) {
 		super( typeName, encoder );
 	}
@@ -57,14 +59,14 @@ public class StringValidationRule extends BaseValidationRule {
 		super( typeName, encoder );
 		addWhitelistPattern( whitelistPattern );
 	}
-	
+
 	/**
 	 * @throws IllegalArgumentException if pattern is null
 	 */
 	public void addWhitelistPattern( String pattern ) {
-		 if (pattern == null) {
-			 throw new IllegalArgumentException("Pattern cannot be null");
-		 }
+		if (pattern == null) {
+			throw new IllegalArgumentException("Pattern cannot be null");
+		}
 		try {
 			whitelistPatterns.add( Pattern.compile( pattern ) );
 		} catch( PatternSyntaxException e ) {
@@ -77,18 +79,18 @@ public class StringValidationRule extends BaseValidationRule {
 	 */
 	public void addWhitelistPattern( Pattern p ) {
 		if (p == null) {
-		   throw new IllegalArgumentException("Pattern cannot be null");
+			throw new IllegalArgumentException("Pattern cannot be null");
 		}
 		whitelistPatterns.add( p );
 	}
-	
+
 	/**
 	 * @throws IllegalArgumentException if pattern is null
 	 */
 	public void addBlacklistPattern( String pattern ) {
-		 if (pattern == null) {
-			 throw new IllegalArgumentException("Pattern cannot be null");
-		 }
+		if (pattern == null) {
+			throw new IllegalArgumentException("Pattern cannot be null");
+		}
 		try {
 			blacklistPatterns.add( Pattern.compile( pattern ) );
 		} catch( PatternSyntaxException e ) {
@@ -100,74 +102,223 @@ public class StringValidationRule extends BaseValidationRule {
 	 * @throws IllegalArgumentException if p is null
 	 */
 	public void addBlacklistPattern( Pattern p ) {
-		 if (p == null) {
-			 throw new IllegalArgumentException("Pattern cannot be null");
-		 }
+		if (p == null) {
+			throw new IllegalArgumentException("Pattern cannot be null");
+		}
 		blacklistPatterns.add( p );
 	}
-	
+
 	public void setMinimumLength( int length ) {
 		minLength = length;
 	}
-	
-	
+
+
 	public void setMaximumLength( int length ) {
 		maxLength = length;
 	}
 
-    /**
-     * {@inheritDoc}
-     */
-	public String getValid( String context, String input ) throws ValidationException {
-	    if ( StringUtils.isEmpty(input) ) {
-			if (allowNull) {
-				return null;
-	    }
-   			throw new ValidationException( this.encoder.encodeForJavaScript(context) + ": Input required.", "Input required: context=" + context + "), input=" + input, context );
-	    }
-	    
-	    // canonicalize
-	    String canonical = null;
-	    try {
-	    	canonical = encoder.canonicalize( input );
-	    } catch (EncodingException e) {
-	        throw new ValidationException( this.encoder.encodeForJavaScript(context) + ": Invalid input. Encoding problem detected.", "Error canonicalizing user input", e, context);
-	    }
+	/**
+	 * Set the flag which determines whether the in input itself is
+	 * checked as well as the canonical form of the input.
+	 * @param flag The value to set
+	 */
+	public void setValidateInputAndCanonical(boolean flag)
+	{
+		validateInputAndCanonical = flag;
+	}
 
-		// check length
-		if (canonical.length() < minLength) {
-			throw new ValidationException( this.encoder.encodeForJavaScript(context) + ": Invalid input. The maximum length of " + maxLength + " characters was exceeded.", "Input exceeds maximum allowed length of " + maxLength + " by " + (canonical.length()-maxLength) + " characters: context=" + context + ", type=" + getTypeName() + "), input=" + input, context );
-		}
-
-		if (canonical.length() > maxLength) {
-			throw new ValidationException( this.encoder.encodeForJavaScript(context) + ": Invalid input. The maximum length of " + maxLength + " characters was exceeded.", "Input exceeds maximum allowed length of " + maxLength + " by " + (canonical.length()-maxLength) + " characters: context=" + context + ", type=" + getTypeName() + ", input=" + input, context );
-		}
-		
+	/**
+	 * checks input against whitelists.
+	 * @param context The context to include in exception messages
+	 * @param input the input to check
+	 * @param orig A origional input to include in exception
+	 *	messages. This is not included if it is the same as
+	 *	input.
+	 * @return input upon a successful check
+	 * @throws ValidationException if the check fails.
+	 */
+	private String checkWhitelist(String context, String input, String orig) throws ValidationException
+	{
 		// check whitelist patterns
 		for (Pattern p : whitelistPatterns) {
-			if ( !p.matcher(canonical).matches() ) {
-    			throw new ValidationException( this.encoder.encodeForJavaScript(context) + ": Invalid input. Please conform to regex " + p.pattern() + ( maxLength == Integer.MAX_VALUE ? "" : " with a maximum length of " + maxLength ), "Invalid input: context=" + context + ", type(" + getTypeName() + ")=" + p.pattern() + ", input=" + input, context );
+			if ( !p.matcher(input).matches() ) {
+				throw new ValidationException( this.encoder.encodeForJavaScript(context) + ": Invalid input. Please conform to regex " + p.pattern() + ( maxLength == Integer.MAX_VALUE ? "" : " with a maximum length of " + maxLength ), "Invalid input: context=" + context + ", type(" + getTypeName() + ")=" + p.pattern() + ", input=" + input + (NullSafe.equals(orig,input) ? "" : ", orig=" + orig), context );
 			}
 		}
-		
+
+		return input;
+	}
+
+	/**
+	 * checks input against whitelists.
+	 * @param context The context to include in exception messages
+	 * @param input the input to check
+	 * @return input upon a successful check
+	 * @throws ValidationException if the check fails.
+	 */
+	private String checkWhitelist(String context, String input) throws ValidationException
+	{
+		return checkWhitelist(context, input, input);
+	}
+
+	/**
+	 * checks input against blacklists.
+	 * @param context The context to include in exception messages
+	 * @param input the input to check
+	 * @param orig A origional input to include in exception
+	 *	messages. This is not included if it is the same as
+	 *	input.
+	 * @return input upon a successful check
+	 * @throws ValidationException if the check fails.
+	 */
+	private String checkBlacklist(String context, String input, String orig) throws ValidationException
+	{
 		// check blacklist patterns
 		for (Pattern p : blacklistPatterns) {
-			if ( p.matcher(canonical).matches() ) {
-    			throw new ValidationException( this.encoder.encodeForJavaScript(context) + ": Invalid input. Dangerous input matching " + p.pattern() + " detected.", "Dangerous input: context=" + context + ", type(" + getTypeName() + ")=" + p.pattern() + ", input=" + input, context );
+			if ( p.matcher(input).matches() ) {
+				throw new ValidationException( this.encoder.encodeForJavaScript(context) + ": Invalid input. Dangerous input matching " + p.pattern() + " detected.", "Dangerous input: context=" + context + ", type(" + getTypeName() + ")=" + p.pattern() + ", input=" + input + (NullSafe.equals(orig,input) ? "" : ", orig=" + orig), context );
 			}
 		}
-		
+
+		return input;
+	}
+
+	/**
+	 * checks input against blacklists.
+	 * @param context The context to include in exception messages
+	 * @param input the input to check
+	 * @return input upon a successful check
+	 * @throws ValidationException if the check fails.
+	 */
+	private String checkBlacklist(String context, String input) throws ValidationException
+	{
+		return checkBlacklist(context, input, input);
+	}
+
+	/**
+	 * checks input lengths
+	 * @param context The context to include in exception messages
+	 * @param input the input to check
+	 * @param orig A origional input to include in exception
+	 *	messages. This is not included if it is the same as
+	 *	input.
+	 * @return input upon a successful check
+	 * @throws ValidationException if the check fails.
+	 */
+	private String checkLength(String context, String input, String orig) throws ValidationException
+	{
+		if (input.length() < minLength) {
+			throw new ValidationException( this.encoder.encodeForJavaScript(context) + ": Invalid input. The maximum length of " + maxLength + " characters was exceeded.", "Input exceeds maximum allowed length of " + maxLength + " by " + (input.length()-maxLength) + " characters: context=" + context + ", type=" + getTypeName() + "), input=" + input + (NullSafe.equals(input,orig) ? "" : ", orig=" + orig), context );
+		}
+
+		if (input.length() > maxLength) {
+			throw new ValidationException( this.encoder.encodeForJavaScript(context) + ": Invalid input. The maximum length of " + maxLength + " characters was exceeded.", "Input exceeds maximum allowed length of " + maxLength + " by " + (input.length()-maxLength) + " characters: context=" + context + ", type=" + getTypeName() + ", orig=" + orig +", input=" + input, context );
+		}
+
+		return input;
+	}
+
+	/**
+	 * checks input lengths
+	 * @param context The context to include in exception messages
+	 * @param input the input to check
+	 * @return input upon a successful check
+	 * @throws ValidationException if the check fails.
+	 */
+	private String checkLength(String context, String input) throws ValidationException
+	{
+		return checkLength(context, input, input);
+	}
+
+	/**
+	 * checks input emptiness
+	 * @param context The context to include in exception messages
+	 * @param input the input to check
+	 * @param orig A origional input to include in exception
+	 *	messages. This is not included if it is the same as
+	 *	input.
+	 * @return input upon a successful check
+	 * @throws ValidationException if the check fails.
+	 */
+	private String checkEmpty(String context, String input, String orig) throws ValidationException
+	{
+		if(!StringUtils.isEmpty(input))
+			return input;
+		if(allowNull)
+			return null;
+		throw new ValidationException( this.encoder.encodeForJavaScript(context) + ": Input required.", "Input required: context=" + context + "), input=" + input + (NullSafe.equals(input,orig) ? "" : ", orig=" + orig), context );
+	}
+
+	/**
+	 * checks input emptiness
+	 * @param context The context to include in exception messages
+	 * @param input the input to check
+	 * @return input upon a successful check
+	 * @throws ValidationException if the check fails.
+	 */
+	private String checkEmpty(String context, String input) throws ValidationException
+	{
+		return checkEmpty(context, input, input);
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public String getValid( String context, String input ) throws ValidationException
+	{
+		String canonical = null;
+
+		// checks on input itself
+
+		// check for empty/null
+		if(checkEmpty(context, input) == null)
+			return null;
+
+		if(validateInputAndCanonical)
+		{
+			// check length
+			checkLength(context, input);
+
+			// check whitelist patterns
+			checkWhitelist(context, input);
+
+			// check blacklist patterns
+			checkBlacklist(context, input);
+		}
+
+		// canonicalize
+		try {
+			canonical = encoder.canonicalize( input );
+		} catch (EncodingException e) {
+			throw new ValidationException( this.encoder.encodeForJavaScript(context) + ": Invalid input. Encoding problem detected.", "Error canonicalizing user input", e, context);
+		}
+
+		// checks on canonical version
+
+		// check for empty/null
+		if(checkEmpty(context, canonical, input) == null)
+			return null;
+
+		// check length
+		checkLength(context, canonical, input);
+
+		// check whitelist patterns
+		checkWhitelist(context, canonical, input);
+
+		// check blacklist patterns
+		checkBlacklist(context, canonical, input);
+
 		// validation passed
 		return canonical;
 	}
 
-    /**
-     * {@inheritDoc}
-     */
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
-	public String sanitize( String context, String input ) {
-		return whitelist( input, EncoderConstants.CHAR_ALPHANUMERICS );
-	}
-	
+		public String sanitize( String context, String input ) {
+			return whitelist( input, EncoderConstants.CHAR_ALPHANUMERICS );
+		}
+
 }
 
