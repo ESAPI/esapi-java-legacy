ce2039b6b18eb107ca3b662d839780e780fb1c49
schallee@darkmist.net <schallee@darkmist.net@69e6d614-4441-0410-9a8b-65af957f44db>
2009-11-14 00:27:36 +0000

    Mark getUniqueReference() final to avoid bugs in subclasses caused by use before initialization possibilities in constructors.

--- trunk/src/main/java/org/owasp/esapi/reference/IntegerAccessReferenceMap.java
+++ trunk/src/main/java/org/owasp/esapi/reference/IntegerAccessReferenceMap.java
@@ -67,8 +67,13 @@ public class IntegerAccessReferenceMap extends AbstractAccessReferenceMap<String
 
 	/**
 	 * TODO Javadoc
+	 * Note: this is final as redefinition by subclasses
+	 * can lead to use before initialization issues as
+	 * {@link #RandomAccessReferenceMap(Set)} and
+	 * {@link #RandomAccessReferenceMap(Set,int)} both call it
+	 * internally.
 	 */
-	protected synchronized String getUniqueReference() {
+	protected final synchronized String getUniqueReference() {
 		return "" + count++;  // returns a string version of the counter
 	}
 
