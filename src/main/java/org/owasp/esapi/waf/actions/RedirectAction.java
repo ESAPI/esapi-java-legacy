463dee2e057271e95294ab3992dfd8f65a612a87
arshan.dabirsiaghi <arshan.dabirsiaghi@69e6d614-4441-0410-9a8b-65af957f44db>
2009-11-20 00:33:25 +0000

    - fixed bug that prevented the block status code from being set (ESAPIWAF.java and RedirectAction.java) - fixed a comment to be, well, understandable - fixed RestrictUserAgentTest to have proper expectations - removed a bean-shell-rule from waf-policy.xml (test file) that screwed up other tests

--- src/main/java/org/owasp/esapi/waf/actions/RedirectAction.java
+++ src/main/java/org/owasp/esapi/waf/actions/RedirectAction.java
@@ -35,15 +35,5 @@ public class RedirectAction extends Action {
 		return this.url;
 	}
 
-	public boolean failedRule() {
-
-		return false;
-	}
-
-	public boolean isActionNecessary() {
-
-		return false;
-	}
-
 
 }
