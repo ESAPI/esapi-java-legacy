2f25205f801193d9f8860cfe4ad4ec2edec80fa2
schallee@darkmist.net <schallee@darkmist.net@69e6d614-4441-0410-9a8b-65af957f44db>
2009-12-09 17:52:02 +0000

    Fix percent encoding issues with non-ascii characters.

--- src/main/java/org/owasp/esapi/codecs/PercentCodec.java
+++ src/main/java/org/owasp/esapi/codecs/PercentCodec.java
@@ -15,6 +15,10 @@
  */
 package org.owasp.esapi.codecs;
 
+import java.io.UnsupportedEncodingException;
+import java.util.Set;
+
+import org.owasp.esapi.util.CollectionsUtil;
 
 /**
  * Implementation of the Codec interface for percent encoding (aka URL encoding).
@@ -24,35 +28,87 @@ package org.owasp.esapi.codecs;
  * @since June 1, 2007
  * @see org.owasp.esapi.Encoder
  */
-public class PercentCodec extends Codec {
+public class PercentCodec extends Codec
+{
+	private static final char[] EMPTY_CHAR_ARRAY = new char[0];
+	private static final String ALPHA_NUMERIC_STR = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+	private static final String RFC3986_RESERVED_STR = ":/?#[]@!$&'()*+,;=";
+	private static final String RFC3986_NON_ALPHANUMERIC_UNRESERVED_STR = "-._~";
+		// rfc3986 2.3: For consistency, percent-encoded octets
+		// in the ranges of ALPHA (%41-%5A and %61-%7A), DIGIT
+		// (%30-%39), hyphen (%2D), period (%2E), underscore
+		// (%5F), or tilde (%7E) should not be created by URI
+		// producers
+	private static final boolean ENCODED_NON_ALPHA_NUMERIC_UNRESERVED = true;
+	private static final String UNENCODED_STR = ALPHA_NUMERIC_STR +
+		(ENCODED_NON_ALPHA_NUMERIC_UNRESERVED ? "" : RFC3986_NON_ALPHANUMERIC_UNRESERVED_STR);
+	private static final Set<Character> UNENCODED_SET = CollectionsUtil.strToUnmodifiableSet(UNENCODED_STR);
 
-	
 	/**
-	 * {@inheritDoc}
-     *
-     * @param immune
-     */
-	public String encodeCharacter( char[] immune, Character c ) {
-
-		// check for immune characters
-		if ( containsCharacter(c, immune ) ) {
-			return ""+c;
+	 * Convinence method to encode a string into UTF-8. This
+	 * wraps the {@link UnsupportedEncodingException} that
+	 * {@link String.getBytes(String)} throws in a
+	 * {@link IllegalStateException} as UTF-8 support is required
+	 * by the Java spec and should never throw this exception.
+	 * @param str the string to encode
+	 * @return str encoded in UTF-8 as bytes.
+	 * @throws IllegalStateException wrapped {@link
+	 *	UnsupportedEncodingException} if
+	 *	{@link String.getBytes(String)} throws it.
+	 */
+	private static byte[] toUtf8Bytes(String str)
+	{
+		try
+		{
+			return str.getBytes("UTF-8");
 		}
-		
-		// check for alphanumeric characters
-		String hex = Codec.getHexForNonAlphanumeric(c);
-		if ( hex == null ) {
-			return ""+c;
+		catch(UnsupportedEncodingException e)
+		{
+			throw new IllegalStateException("The Java spec requires UTF-8 support.", e);
 		}
-		
-        if (c < 0x10) {
-            hex = '0' + hex;
-        }
-	    return '%' + hex;
 	}
-	
-	
-	
+
+	/**
+	 * Append the two upper case hex characters for a byte.
+	 * @param sb The string buffer to append to.
+	 * @param b The byte to hexify
+	 * @returns sb with the hex characters appended.
+	 */
+	// rfc3986 2.1: For consistency, URI producers and normalizers
+	// should use uppercase hexadecimal digits for all percent-
+	// encodings.
+	private static StringBuilder appendTwoUpperHex(StringBuilder sb, int b)
+	{
+		if(b < Byte.MIN_VALUE || b > Byte.MAX_VALUE)
+			throw new IllegalArgumentException("b is not a byte (was " + b + ')');
+		b &= 0xFF;
+		if(b<0x10)
+			sb.append('0');
+		return sb.append(Integer.toHexString(b).toUpperCase());
+	}
+
+	/**
+	 * Encode a character for URLs
+	 * @param immune characters not to encode
+	 * @param c character to encode
+	 * @return the encoded string representing c
+	 */
+	public String encodeCharacter( char[] immune, Character c )
+	{
+		String cStr = String.valueOf(c.charValue());
+		byte[] bytes;
+		StringBuilder sb;
+
+		if(UNENCODED_SET.contains(c))
+			return cStr;
+
+		bytes = toUtf8Bytes(cStr);
+		sb = new StringBuilder(bytes.length * 3);
+		for(byte b : bytes)
+			appendTwoUpperHex(sb.append('%'), b);
+		return sb.toString();
+	}
+
 	/**
 	 * {@inheritDoc}
 	 * 
@@ -69,13 +125,13 @@ public class PercentCodec extends Codec {
 			input.reset();
 			return null;
 		}
-		
+
 		// if this is not an encoded character, return null
 		if (first != '%' ) {
 			input.reset();
 			return null;
 		}
-				
+
 		// Search for exactly 2 hex digits following
 		StringBuilder sb = new StringBuilder();
 		for ( int i=0; i<2; i++ ) {
@@ -86,13 +142,13 @@ public class PercentCodec extends Codec {
 			try {
 				// parse the hex digit and create a character
 				int i = Integer.parseInt(sb.toString(), 16);
-                if (Character.isValidCodePoint(i)) {
-                    return (char) i;
-			}
-            } catch( NumberFormatException ignored ) { }
+				if (Character.isValidCodePoint(i)) {
+					return (char) i;
+				}
+			} catch( NumberFormatException ignored ) { }
 		}
 		input.reset();
 		return null;
 	}
-	
+
 }
