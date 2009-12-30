599f2d59efb269807692d81961d1c77c009d6d55
kevin.w.wall <kevin.w.wall@69e6d614-4441-0410-9a8b-65af957f44db>
2009-12-30 03:28:48 +0000

    Changes so that the testUnicodeString() test case will be skipped if unicodeStr and altString are not equal at runtime. (They should be.) This is meant to deal with the Windows-1252 vs. UTF-8 encoding issue on Windows OS. Also changed testOverwrite() so it did not depend on both unicodeStr and altString. Now only uses unicodeStr.

--- src/test/java/org/owasp/esapi/PlainTextTest.java
+++ src/test/java/org/owasp/esapi/PlainTextTest.java
@@ -15,9 +15,32 @@ public class PlainTextTest {
 	private String unicodeStr = "A\u00ea\u00f1\u00fcC";	// I.e., "AêñüC"
 	private String altString  = "AêñüC";				// Same as above.
 	
+	/* NOTE: This test will not work on Windows unless executed under
+	 * Eclipse and the file is stored / treated as a UTF-8 encoded file
+	 * rather than the Windows native OS encoding of Windows-1252 (aka,
+	 * CP-1252). Therefore this test case has a check to not run the test
+	 * unless
+	 *     unicodeStr.equals(altString)
+	 * is true. If not the test is skipped and a message is printed to stderr.
+	 * Jim Manico made an attempt to address this (see private email to
+	 * kevin.w.wall@gmail.com on 11/26/2009, subject "Re: [OWASP-ESAPI] Unit
+	 * Tests Status") to correct this problem by setting some SVN attribute
+	 * to standardize all source files to UTF-8, but not all Subversion clients
+	 * are either honoring this or perhaps Windows just overrides this. Either
+	 * way, this test (which used to be an assertTrue() expression) was
+	 * introduced to account for this.
+	 */
 	@Test
 	public final void testUnicodeString() {
-		assertTrue(unicodeStr.equals(altString));
+	    // These 2 strings are *meant* to be equal. If they are not, please
+	    // do *NOT* change the test. It's a Windows thing. Sorry. Change your
+	    // OS instead. ;-)
+	    if ( ! unicodeStr.equals(altString) ) {
+	        System.err.println("Skipping JUnit test case " +
+	                           "PlainTextTest.testUnicodeString() on OS " +
+	                           System.getProperty("os.name") );
+	        return;
+	    }
 		try {
 			byte[] utf8Bytes = unicodeStr.getBytes("UTF-8");
 			PlainText pt1 = new PlainText(unicodeStr);
@@ -46,9 +69,9 @@ public class PlainTextTest {
 	@Test
 	public final void testOverwrite() {
 		try {
-			byte[] utf8Bytes = altString.getBytes("UTF-8");
+			byte[] utf8Bytes = unicodeStr.getBytes("UTF-8");
 			PlainText pt = new PlainText(utf8Bytes);
-			assertTrue( pt.toString().equals(altString) );
+			assertTrue( pt.toString().equals(unicodeStr) );
 			assertTrue( Arrays.equals(utf8Bytes, pt.asBytes()) );			
 			assertTrue( pt.hashCode() == unicodeStr.hashCode() );
 			
