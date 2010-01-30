8eb36b385ffc11508357d15b55ebcc875fd697cb
manico.james <manico.james@69e6d614-4441-0410-9a8b-65af957f44db>
2010-01-30 04:43:31 +0000

    fixing isValidDouble

--- branches/1.4/src/main/java/org/owasp/esapi/reference/DefaultValidator.java
+++ branches/1.4/src/main/java/org/owasp/esapi/reference/DefaultValidator.java
@@ -612,7 +612,12 @@ public class DefaultValidator implements org.owasp.esapi.Validator {
 	 * 
 	 */
 	public boolean isValidDouble(String context, String input, double minValue, double maxValue, boolean allowNull) throws IntrusionException {
-		return isValidDouble( context, input, minValue, maxValue, allowNull );
+        try {
+            getValidDouble( context, input, minValue, maxValue, allowNull );
+            return true;
+        } catch( Exception e ) {
+            return false;
+        }
 	}
 	
 	/**
