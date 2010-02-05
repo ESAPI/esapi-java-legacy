7025e2ce9ace1e3b9ba9d547db12cbabe89cff63
kevin.w.wall <kevin.w.wall@69e6d614-4441-0410-9a8b-65af957f44db>
2010-02-05 23:52:58 +0000

    Make findbugs shutup about false positive. It was warning that 'bis' in use of 'bis.close()' in finally block was *always* null, which it wasn't.

--- src/main/java/org/owasp/esapi/codecs/Base64.java
+++ src/main/java/org/owasp/esapi/codecs/Base64.java
@@ -1244,7 +1244,7 @@ public class Base64
         }   // end catch: IOException
         finally
         {
-            try{ bis.close(); } catch( Exception e) {}
+            try{ if (bis != null ) bis.close(); } catch( Exception e) {}
         }   // end finally
         
         return decodedData;
