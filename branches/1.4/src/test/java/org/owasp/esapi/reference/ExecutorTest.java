bd4b05ecb9c89d91822e6341a14302c0f186281a
schallee@darkmist.net <schallee@darkmist.net@69e6d614-4441-0410-9a8b-65af957f44db>
2010-02-08 12:54:27 +0000

    Fix one more hard coded path in ValidatorTest (thanks to Brent Shikoski for catching the one I missed).
    Remove old hard coded path tests from ExecutorTest (thanks for reviewing
    this Jim).

--- branches/1.4/src/test/java/org/owasp/esapi/reference/ExecutorTest.java
+++ branches/1.4/src/test/java/org/owasp/esapi/reference/ExecutorTest.java
@@ -65,7 +65,9 @@ public class ExecutorTest extends TestCase
 		}
 		catch(IOException e)
 		{
-			throw new IllegalStateException("IOException setting static Files that should exist.");//, e);
+			IllegalStateException wrapper = new IllegalStateException("IOException setting static Files that should exist.");
+			wrapper.initCause(e);
+			throw wrapper;
 		}
 	}
 
@@ -157,153 +159,6 @@ public class ExecutorTest extends TestCase
 	public static Test suite() {
 		TestSuite suite = new TestSuite(ExecutorTest.class);
 		return suite;
-	}
-
-	/**
-	 * Test of executeOSCommand method, of class org.owasp.esapi.Executor
-	 * 
-	 * @throws Exception
-	 *             the exception
-	 */
-	public void testExecuteWindowsSystemCommand() throws Exception {
-		System.out.println("executeWindowsSystemCommand");
-
-		if (!IS_WINDOWS)
-		{
-			System.out.println("testExecuteWindowsSystemCommand - on non-Windows platform, exiting");
-			return;	// Not windows, not going to execute this path
-		}
-
-		Codec codec = new WindowsCodec();
-		System.out.println("executeSystemCommand");
-		Executor instance = ESAPI.executor();
-		File executable = new File( "C:\\Windows\\System32\\cmd.exe" );
-		File working = new File("C:\\");
-		List params = new ArrayList();
-		try {
-			params.add("/C");
-			params.add("dir");
-			String result = instance.executeSystemCommand(executable, new ArrayList(params), working, codec);
-			System.out.println( "RESULT: " + result );
-			assertTrue(result.length() > 0);
-		} catch (Exception e) {
-			fail(e.getMessage());
-		}
-		try {
-			File exec2 = new File( executable.getPath() + ";inject.exe" );
-			String result = instance.executeSystemCommand(exec2, new ArrayList(params), working, codec);
-			System.out.println( "RESULT: " + result );
-			fail();
-		} catch (Exception e) {
-			// expected
-		}
-		try {
-			File exec2 = new File( executable.getPath() + "\\..\\cmd.exe" );
-			String result = instance.executeSystemCommand(exec2, new ArrayList(params), working, codec);
-			System.out.println( "RESULT: " + result );
-			fail();
-		} catch (Exception e) {
-			// expected
-		}
-		try {
-			File workdir = new File( "ridiculous" );
-			String result = instance.executeSystemCommand(executable, new ArrayList(params), workdir, codec);
-			System.out.println( "RESULT: " + result );
-			fail();
-		} catch (Exception e) {
-			// expected
-		}
-		try {
-			params.add("&dir");
-			String result = instance.executeSystemCommand(executable, new ArrayList(params), working, codec);
-			System.out.println( "RESULT: " + result );
-		} catch (Exception e) {
-			fail();
-		}
-
-		try {
-			params.set( params.size()-1, "c:\\autoexec.bat" );
-			String result = instance.executeSystemCommand(executable, new ArrayList(params), working, codec);
-			System.out.println( "RESULT: " + result );
-		} catch (Exception e) {
-			fail();
-		}
-
-		try {
-			params.set( params.size()-1, "c:\\autoexec.bat c:\\config.sys" );
-			String result = instance.executeSystemCommand(executable, new ArrayList(params), working, codec);
-			System.out.println( "RESULT: " + result );
-		} catch (Exception e) {
-			fail();
-		}
-	}
-
-	/**
-	 * Test of executeOSCommand method, of class org.owasp.esapi.Executor
-	 * 
-	 * @throws Exception
-	 *             the exception
-	 */
-	public void testExecuteUnixSystemCommand() throws Exception {
-		System.out.println("executeUnixSystemCommand");
-		File workingDir = new File("/tmp");
-
-		if (IS_WINDOWS)
-		{
-			System.out.println("executeUnixSystemCommand - on Windows platform, exiting");
-			return;
-		}
-
-		// FIXME: need more test cases to use this codec
-		Codec codec = new UnixCodec();
-
-		// make sure we have what /bin/sh is pointing at in the allowed exes for the test
-		// and a usable working dir
-		File binSh = new File("/bin/sh").getCanonicalFile();
-		ESAPI.setSecurityConfiguration(
-				new Conf(
-					ESAPI.securityConfiguration(),
-					Collections.singletonList(binSh.getPath()),
-					workingDir
-					)
-				);
-
-		Executor instance = ESAPI.executor();
-		File executable = binSh;
-		List params = new ArrayList();
-		try {
-			params.add("-c");
-			params.add("ls");
-			params.add("/");
-			String result = instance.executeSystemCommand(executable, new ArrayList(params), workingDir, codec);
-			System.out.println( "RESULT: " + result );
-			assertTrue(result.length() > 0);
-		} catch (Exception e) {
-			fail(e.getMessage());
-		}
-		try {
-			File exec2 = new File( executable.getPath() + ";./inject" );
-			String result = instance.executeSystemCommand(exec2, new ArrayList(params), workingDir, codec);
-			System.out.println( "RESULT: " + result );
-			fail();
-		} catch (Exception e) {
-			// expected
-		}
-		try {
-			File exec2 = new File( executable.getPath() + "/../bin/sh" );
-			String result = instance.executeSystemCommand(exec2, new ArrayList(params), workingDir, codec);
-			System.out.println( "RESULT: " + result );
-			fail();
-		} catch (Exception e) {
-			// expected
-		}
-		try {
-			params.add(";ls");
-			String result = instance.executeSystemCommand(executable, new ArrayList(params), workingDir, codec);
-			System.out.println( "RESULT: " + result );
-		} catch (Exception e) {
-			fail();
-		}
 	}
 
 	public void testExecuteJava() throws Exception
