42aebf84a5d1e81b5111fdbc75538e557441dbc0
manico.james <manico.james@69e6d614-4441-0410-9a8b-65af957f44db>
2010-02-24 01:18:02 +0000

    provided features where log4j can be extended to remove dependencies on the ESAPI request and user objects.

--- trunk/src/main/java/org/owasp/esapi/reference/ExtendedLog4JLogFactory.java
+++ trunk/src/main/java/org/owasp/esapi/reference/ExtendedLog4JLogFactory.java
@@ -1,14 +1,42 @@
 package org.owasp.esapi.reference;
 
+import org.owasp.esapi.Logger;
+
+/**
+ * 
+ * This class demonstrates how to extend the Log4JLogFactory. 
+ * 
+ * @author jmanico
+ *
+ */
 public class ExtendedLog4JLogFactory extends Log4JLogFactory {
+
+	protected static class ExtendedLog4JLogger extends Log4JLogger {
+		protected ExtendedLog4JLogger(String moduleName) {
+            super(moduleName);
+        }
+		
+		/**
+		 * Please replace this function with your own. 
+		 * 
+		 * The reference implementation includes the users account name, last host
+		 * address, and a secure replacement for the session id. 
+		 * 
+		 **/
+		public String getUserInfo() {
+			return "-EXTENDEDUSERINFO-";
+		}
+	}
 	
-	/**
-	 * In the Javadoc for this getUserInfo() method (or whatever you call it, can you explain everything that we include, user, 
-	 * source host:port, dest IP port, target URL], and indicate they might want to add or subtract from this in their version.
-	 * 
-	 * @return
-	 */
-    public String getUserInfo() {
-		return "JIMINFOTEST";
+	public Logger getLogger(String moduleName) {
+    	
+    	// If a logger for this module already exists, we return the same one, otherwise we create a new one.
+    	Logger moduleLogger = (Logger) loggersMap.get(moduleName);
+    	
+    	if (moduleLogger == null) {
+    		moduleLogger = new ExtendedLog4JLogger(moduleName);
+    		loggersMap.put(moduleName, moduleLogger);    		
+    	}
+		return moduleLogger;
     }
 }
