7f6fe25dd85a98aba64d417498040023f7aa0877
kevin.w.wall <kevin.w.wall@69e6d614-4441-0410-9a8b-65af957f44db>
2010-03-01 03:51:47 +0000

    Bash script to execute JavaEncryptor to assist with setting of MasterKey and MasterSalt values.

---
+++ src/examples/scripts/setMasterKey.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+findjar=$PWD/findjar.sh
+
+cd ../../../
+esapi_props=
+
+if [[ -f configuration/.esapi/ESAPI.properties ]]
+then    esapi_props="configuration/.esapi/ESAPI.properties"
+else    esapi_props="src/main/resources/.esapi/ESAPI.properties"
+fi
+if [[ ! -r $esapi_props ]]
+then    echo "$0: ESAPI.properties file ($esapi_props) not readable." >&2
+        exit 1
+fi
+
+# This classpath is relative to ../../.. directory (from here). See above 'cd'
+classpath=".:target/ESAPI-2.0.jar:\
+$($findjar log4j-1.2.12.jar):\
+$($findjar commons-fileupload-1.2.jar):\
+$($findjar servlet-api-2.4.jar)"
+
+set -x
+java -Dlog4j.configuration=src/test/resources/log4j.xml \
+     -Dorg.owasp.esapi.resources=$esapi_props \
+     -classpath $classpath \
+     org.owasp.esapi.reference.crypto.JavaEncryptor "$@"
