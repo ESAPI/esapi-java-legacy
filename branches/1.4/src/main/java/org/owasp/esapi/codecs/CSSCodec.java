b03fe978d1e2970e09ca7a896523a466e67d79e9
schallee@darkmist.net <schallee@darkmist.net@69e6d614-4441-0410-9a8b-65af957f44db>
2010-05-02 21:31:41 +0000

    Backport CSS decoding changes from 2.0 to 1.4. Also make PushbackString#isHexDigit(char) static.

--- branches/1.4/src/main/java/org/owasp/esapi/codecs/CSSCodec.java
+++ branches/1.4/src/main/java/org/owasp/esapi/codecs/CSSCodec.java
@@ -33,6 +33,7 @@ public class CSSCodec implements Codec
 	private static final String ALPHA_NUMERIC_STR = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
 	private static final String UNENCODED_STR = ALPHA_NUMERIC_STR + " \t";
 	private static final Set/*<Character>*/ UNENCODED_SET = CollectionsUtil.strToUnmodifiableSet(UNENCODED_STR);
+	private static final Character REPLACEMENT = PrimWrap.wrapChar('\ufffd');
 
 	/**
 	 * Public Constructor for CSSCodec
@@ -96,21 +97,16 @@ public class CSSCodec implements Codec
 
 	/**
 	 * {@inheritDoc}
-	 * Returns the decoded version of the character starting at index, or null if no decoding is
-	 * possible.
 	 * 
-	 * Formats all are legal both upper/lower case: \\x - special characters \\HHHH
+	 * Returns the decoded version of the character starting at index,
+	 * or null if no decoding is possible.
 	 */
-	public Character decodeCharacter(PushbackString input) {
+	public Character decodeCharacter(PushbackString input)
+	{
 		input.mark();
 		Character first = input.next();
-		if (first == null) {
-			input.reset();
-			return null;
-		}
-
-		// if this is not an encoded character, return null
-		if (first.charValue() != '\\') {
+		if (first == null || first.charValue() != '\\')
+		{
 			input.reset();
 			return null;
 		}
@@ -121,36 +117,109 @@ public class CSSCodec implements Codec
 			return null;
 		}
 
+		/* From css 2.1 spec:
+		 * http://www.w3.org/TR/CSS21/syndata.html#characters
+		 *
+		 * First, inside a string, a backslash followed by a
+		 * newline is ignored (i.e., the string is deemed not
+		 * to contain either the backslash or the newline).
+		 *
+		 * Second, it cancels the meaning of special CSS
+		 * characters. Except within CSS comments, any character
+		 * (except a hexadecimal digit, linefeed, carriage return,
+		 * or form feed) can be escaped with a backslash to
+		 * remove its special meaning. For example, "\"" is a string
+		 * consisting of one double quote. Style sheet
+		 * preprocessors must not remove these backslashes
+		 * from a style sheet since that would change the style
+		 * sheet's meaning.
+		 *
+		 * Third, backslash escapes allow authors to refer to
+		 * characters they cannot easily put in a document. In
+		 * this case, the backslash is followed by at most six
+		 * hexadecimal digits (0..9A..F), which stand for the ISO
+		 * 10646 ([ISO10646]) character with that number, which
+		 * must not be zero. (It is undefined in CSS 2.1 what
+		 * happens if a style sheet does contain a character with
+		 * Unicode codepoint zero.) If a character in the range
+		 * [0-9a-fA-F] follows the hexadecimal number, the end
+		 * of the number needs to be made clear. There are two
+		 * ways to do that:
+		 *
+		 *	1. with a space (or other white space character):
+		 *	"\26 B" ("&B"). In this case, user agents should
+		 *	treat a "CR/LF" pair (U+000D/U+000A) as a single
+		 *	white space character.
+		 *
+		 *	2. by providing exactly 6 hexadecimal digits:
+		 *	"\000026B" ("&B")
+		 *
+		 * In fact, these two methods may be combined. Only one
+		 * white space character is ignored after a hexadecimal
+		 * escape. Note that this means that a "real" space
+		 * after the escape sequence must itself either be
+		 * escaped or doubled.
+		 *
+		 * If the number is outside the range allowed by Unicode
+		 * (e.g., "\110000" is above the maximum 10FFFF allowed in
+		 * current Unicode), the UA may replace the escape with
+		 * the "replacement character" (U+FFFD). If the character
+		 * is to be displayed, the UA should show a visible
+		 * symbol, such as a "missing character" glyph (cf. 15.2,
+		 * point 5).
+		 */
 
-		// look for \HHH format
-		if (input.isHexDigit(second)) {
-			// Search for up to 6 hex digits following until a space
-			StringBuffer sb = new StringBuffer();
-			sb.append(second);
-			for (int i = 0; i < 5; i++) {
-				Character c = input.next();
-				if (c == null || c.charValue() == 0x20)
-					break;
-				if (input.isHexDigit(c)) {
-					sb.append(c);
-				} else {
-					input.pushback(c);
-					break;
-				}
-			}
-			try {
-				// parse the hex digit and create a character
-				int i = Integer.parseInt(sb.toString(), 16);
-				// TODO: in Java 1.5 you can test whether this is a valid code point
-				// with Character.isValidCodePoint() et al.
-				return new Character((char) i);
-			} catch (NumberFormatException e) {
-				// throw an exception for malformed entity?
-				// just continue which will reset and return null
-			}
+		switch(second.charValue())
+		{	// special whitespace cases. I assume they mean
+			// for all of these to qualify as a "new
+			// line." Otherwise there is no specification
+			// of what to do for \f
+			case '\r':
+				if(input.peek('\n'))
+					input.next();
+				// fall through
+			case '\n':
+			case '\f':
+				// bs follwed by new line replaced by nothing
+			case '\u0000':	// skip NUL for now too
+				return decodeCharacter(input);
 		}
 
-		return second;
-	}
+		if (!PushbackString.isHexDigit(second))
+		{	// non hex digit
+			return second;
+		}
 
+		// Search for up to 6 hex digits following until a space
+		StringBuffer sb = new StringBuffer();
+		sb.append(second);
+		for (int i = 0; i < 5; i++)
+		{
+			Character c = input.next();
+			if(c == null || Character.isWhitespace(c.charValue()))
+				break;
+			if(PushbackString.isHexDigit(c))
+				sb.append(c);
+			else
+			{
+				input.pushback(c);
+				break;
+			}
+		}
+		try
+		{
+			// parse the hex digit and create a character
+			int i = Integer.parseInt(sb.toString(), 16);
+	
+			if (i <= 0x10ffff)
+				return PrimWrap.wrapChar((char)i);
+			return REPLACEMENT;
+		}
+		catch (NumberFormatException e)
+		{
+			IllegalStateException ise = new IllegalStateException("Received a NumberFormateException parsing a string verified to be hex");
+			ise.initCause(e);
+			throw ise;
+        	}
+	}
 }
