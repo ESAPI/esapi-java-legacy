08c345e744ecf4ebfa52ce97aee2266ee44c6b19
schallee@darkmist.net <schallee@darkmist.net@69e6d614-4441-0410-9a8b-65af957f44db>
2010-05-16 01:50:26 +0000

    Fix SecurityWrapperRequest to handle null properly in getQueryString(). Fixes issue 121
    Add setQueryString() to MockHttpServletRequest for testing above.
    
    Add tests to SafeRequestTtest for above.
    
    Fix regular expressions in both main and test to work and add % to the list of characters.
    
    See issue 125 for further issues regarding getQueryString()

--- trunk/src/test/java/org/owasp/esapi/filters/SafeRequestTest.java
+++ trunk/src/test/java/org/owasp/esapi/filters/SafeRequestTest.java
@@ -33,26 +33,26 @@ public class SafeRequestTest extends TestCase {
 	 * Instantiates a new access controller test.
 	 * 
 	 * @param testName
-     *            the test name
-     * @throws Exception
+	 *            the test name
+	 * @throws Exception
 	 */
 	public SafeRequestTest(String testName) throws Exception {
 		super(testName);
 	}
 
-    /**
-     * {@inheritDoc}
-     *
-     * @throws Exception
-     */
+	/**
+	 * {@inheritDoc}
+	 *
+	 * @throws Exception
+	 */
 	protected void setUp() throws Exception {
 	}
 
-    /**
-     * {@inheritDoc}
-     *
-     * @throws Exception
-     */
+	/**
+	 * {@inheritDoc}
+	 *
+	 * @throws Exception
+	 */
 	protected void tearDown() throws Exception {
 		// none
 	}
@@ -67,22 +67,93 @@ public class SafeRequestTest extends TestCase {
 		return suite;
 	}
 
-    /**
-     *
-     */
-    public void testGetRequestParameters() {
-    	System.out.println( "getRequestParameters");
-        MockHttpServletRequest request = new MockHttpServletRequest();
-        request.addParameter( "one","1" );
-        request.addParameter( "two","2" );
-        request.addParameter( "one","3" );
-        request.addParameter( "one","4" );
-        SecurityWrapperRequest safeRequest = new SecurityWrapperRequest( request );
-        String[] params = safeRequest.getParameterValues("one");
-        String out = "";
-        for (int i = 0; i < params.length; i++ ) out += params[i];
-        assertEquals( "134", out );
+	/**
+	 *
+	 */
+	public void testGetRequestParameters() {
+		System.out.println( "getRequestParameters");
+		MockHttpServletRequest request = new MockHttpServletRequest();
+		request.addParameter( "one","1" );
+		request.addParameter( "two","2" );
+		request.addParameter( "one","3" );
+		request.addParameter( "one","4" );
+		SecurityWrapperRequest safeRequest = new SecurityWrapperRequest( request );
+		String[] params = safeRequest.getParameterValues("one");
+		String out = "";
+		for (int i = 0; i < params.length; i++ ) out += params[i];
+		assertEquals( "134", out );
+	}
+
+	public void testGetQueryStringNull()
+	{
+		MockHttpServletRequest req = new MockHttpServletRequest();
+		SecurityWrapperRequest wrappedReq;
+
+		req.setQueryString(null);
+		wrappedReq = new SecurityWrapperRequest(req);
+		assertNull(wrappedReq.getQueryString());
+	}
+
+	public void testGetQueryStringNonNull()
+	{
+		MockHttpServletRequest req = new MockHttpServletRequest();
+		SecurityWrapperRequest wrappedReq;
+
+		req.setQueryString("a=b");
+		wrappedReq = new SecurityWrapperRequest(req);
+		assertEquals("a=b",wrappedReq.getQueryString());
+	}
+
+	public void testGetQueryStringNUL()
+	{
+		MockHttpServletRequest req = new MockHttpServletRequest();
+		SecurityWrapperRequest wrappedReq;
+
+		req.setQueryString("a=\u0000");
+		wrappedReq = new SecurityWrapperRequest(req);
+		assertEquals("",wrappedReq.getQueryString());
+	}
+
+	public void testGetQueryStringPercent()
+	{
+		MockHttpServletRequest req = new MockHttpServletRequest();
+		SecurityWrapperRequest wrappedReq;
+
+		req.setQueryString("a=%62");
+		wrappedReq = new SecurityWrapperRequest(req);
+		assertEquals("a=b",wrappedReq.getQueryString());
 	}
 	
+	public void testGetQueryStringPercentNUL()
+	{
+		MockHttpServletRequest req = new MockHttpServletRequest();
+		SecurityWrapperRequest wrappedReq;
 
+		req.setQueryString("a=%00");
+		wrappedReq = new SecurityWrapperRequest(req);
+		assertEquals("",wrappedReq.getQueryString());
+	}
+
+	/* these tests need to be enabled&changed based on the decisions
+	 * made regarding issue 125. Currently they fail.
+	public void testGetQueryStringPercentEquals()
+	{
+		MockHttpServletRequest req = new MockHttpServletRequest();
+		SecurityWrapperRequest wrappedReq;
+
+		req.setQueryString("a=%3d");
+		wrappedReq = new SecurityWrapperRequest(req);
+		assertEquals("a=%3d",wrappedReq.getQueryString());
+	}
+
+	public void testGetQueryStringPercentAmpersand()
+	{
+		MockHttpServletRequest req = new MockHttpServletRequest();
+		SecurityWrapperRequest wrappedReq;
+
+		req.setQueryString("a=%26b");
+		wrappedReq = new SecurityWrapperRequest(req);
+		assertEquals("a=%26b",wrappedReq.getQueryString());
+	}
+	*/
 }
