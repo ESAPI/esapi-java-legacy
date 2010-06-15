2f6a8dd03fefb9de43047a541648c21928da01d4
manico.james <manico.james@69e6d614-4441-0410-9a8b-65af957f44db>
2010-06-15 21:50:53 +0000

    updated isSessionAbsoluteTimeout and isSessionTimeout to convert configured minutes to miliseconds

--- src/main/java/org/owasp/esapi/reference/DefaultUser.java
+++ src/main/java/org/owasp/esapi/reference/DefaultUser.java
@@ -356,7 +356,7 @@ public class DefaultUser implements User, Serializable {
 	public boolean isSessionAbsoluteTimeout() {
 		HttpSession session = ESAPI.httpUtilities().getCurrentRequest().getSession(false);
 		if ( session == null ) return true;
-		Date deadline = new Date( session.getCreationTime() + ABSOLUTE_TIMEOUT_LENGTH);
+		Date deadline = new Date( session.getCreationTime() + (1000 * 60 * 60 * ABSOLUTE_TIMEOUT_LENGTH));
 		Date now = new Date();
 		return now.after(deadline);
 	}
@@ -367,7 +367,7 @@ public class DefaultUser implements User, Serializable {
 	public boolean isSessionTimeout() {
 		HttpSession session = ESAPI.httpUtilities().getCurrentRequest().getSession(false);
 		if ( session == null ) return true;
-		Date deadline = new Date(session.getLastAccessedTime() + IDLE_TIMEOUT_LENGTH);
+		Date deadline = new Date( session.getLastAccessedTime() + (1000 * 60 * 60 * IDLE_TIMEOUT_LENGTH));
 		Date now = new Date();
 		return now.after(deadline);
 	}
