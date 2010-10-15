e70cdba44be8005aa5f99579769114a1b426f2bd
chrisisbeef <chrisisbeef@69e6d614-4441-0410-9a8b-65af957f44db>
2010-10-15 19:42:58 +0000

    Mistagged while configuring maven-release-plugin

--- 2.0_rc10/src/main/java/org/owasp/esapi/reference/accesscontrol/AlwaysTrueACR.java
+++
@@ -1,9 +0,0 @@
-package org.owasp.esapi.reference.accesscontrol;
-
-public class AlwaysTrueACR extends BaseACR<Object, Object> {
-	
-//	@Override
-	public boolean isAuthorized(Object runtimeParameter) {
-		return true;
-	}
-}
