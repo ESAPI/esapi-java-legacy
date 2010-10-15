ed149f5b0f1ff8f004544c8adbe27e1552cfb8ba
chrisisbeef <chrisisbeef@69e6d614-4441-0410-9a8b-65af957f44db>
2010-10-15 21:12:30 +0000

    Working on Release - fine tuning pom.xml

--- tags/esapi-2.0_rc10/src/main/java/org/owasp/esapi/reference/accesscontrol/policyloader/ACRParameterLoader.java
+++
@@ -1,9 +0,0 @@
-package org.owasp.esapi.reference.accesscontrol.policyloader;
-
-import org.apache.commons.configuration.XMLConfiguration;
-
-
-public interface ACRParameterLoader <T> {
-	public abstract T getParameters(XMLConfiguration config, int currentRule)
-		throws java.lang.Exception; //TODO this exception could be more specific
-}
