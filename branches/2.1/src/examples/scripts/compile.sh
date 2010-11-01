c72e0f4d9d78f956dca624e7a1224b778034d377
manico.james <manico.james@69e6d614-4441-0410-9a8b-65af957f44db>
2010-11-01 03:28:45 +0000

    deleting 2.1 contents

--- branches/2.1/src/examples/scripts/compile.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-cd ../java
-javac -classpath .:../../../target/ESAPI-2.0.jar *.java
--- branches/2.1/src/main/java/org/owasp/esapi/AccessControlRule.java
+++
@@ -1,8 +0,0 @@
-package org.owasp.esapi;
-
-
-public interface AccessControlRule<P, R> {
-	public void setPolicyParameters(P policyParameter);
-	public P getPolicyParameters();
-	public boolean isAuthorized(R runtimeParameter) throws Exception;
-}
