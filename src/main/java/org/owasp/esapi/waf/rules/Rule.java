6a7f6ba25545cbf4d394fbed34b8e1a2fabb19de
arshan.dabirsiaghi <arshan.dabirsiaghi@69e6d614-4441-0410-9a8b-65af957f44db>
2010-11-16 03:55:11 +0000

    - made ConfigurationException extend EnterpriseSecurityException and made the necessary upgrades throughout the config parsing code to support that - changed usage of log4j to ESAPI logger (there are still references to log4j in the imports because of the old APIs i just deprecated rather than removed)

--- src/main/java/org/owasp/esapi/waf/rules/Rule.java
+++ src/main/java/org/owasp/esapi/waf/rules/Rule.java
@@ -16,9 +16,11 @@
 package org.owasp.esapi.waf.rules;
 
 import javax.servlet.http.HttpServletRequest;
+
 import javax.servlet.http.HttpServletResponse;
 
-import org.apache.log4j.Logger;
+import org.owasp.esapi.ESAPI;
+import org.owasp.esapi.Logger;
 import org.owasp.esapi.waf.actions.Action;
 import org.owasp.esapi.waf.configuration.AppGuardianConfiguration;
 import org.owasp.esapi.waf.internal.InterceptingHTTPServletResponse;
@@ -31,16 +33,13 @@ import org.owasp.esapi.waf.internal.InterceptingHTTPServletResponse;
 public abstract class Rule {
 
 	protected String id = "(no rule ID)";
-	protected static Logger logger = Logger.getLogger(Rule.class);
+	protected static Logger logger = ESAPI.getLogger(Rule.class);
 
 	public abstract Action check( HttpServletRequest request, InterceptingHTTPServletResponse response, HttpServletResponse httpResponse );
 
 	public void log( HttpServletRequest request, String message ) {
-
-		logger.log(AppGuardianConfiguration.LOG_LEVEL,
-				"[IP=" + request.getRemoteAddr() +
+		logger.warning(Logger.SECURITY_FAILURE,"[IP=" + request.getRemoteAddr() +
 				",Rule=" + this.getClass().getSimpleName() + ",ID="+id+"] " + message);
-
 	}
 
 	protected void setId(String id) {
