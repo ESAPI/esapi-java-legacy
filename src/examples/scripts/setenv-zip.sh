d6512f27c79f526d341af2dff850e32587303266
kevin.w.wall <kevin.w.wall@69e6d614-4441-0410-9a8b-65af957f44db>
2011-02-04 05:17:20 +0000

    Update script to work with ESAPI 2.0_rc10 and later. Improve output.

--- src/examples/scripts/setenv-zip.sh
+++ src/examples/scripts/setenv-zip.sh
@@ -13,25 +13,25 @@
 # since the specific version of the library is delivered as part of the
 # ESAPI zip file. In this manner, we do not have to update this if these
 # versions change. For the record, at the time of this writing, these were
-# log4j-1.2.12.jar, commons-fileupload-1.2.jar, and servlet-api-2.4.jar.
+# log4j-1.2.16.jar, commons-fileupload-1.2.jar, and servlet-api-2.4.jar.
 esapi_classpath=".:\
 $(ls ../../../esapi*.jar):\
 $(./findjar.sh -start ../../../libs log4j-*.jar):\
 $(./findjar.sh -start ../../../libs commons-fileupload-*.jar):\
 $(./findjar.sh -start ../../../libs servlet-api-*.jar)"
 
-esapi_resources="$(\cd ../../../configuration/.esapi >&- 2>&- && pwd)"
-esapi_resources_test="$(\cd ../../test/resources/.esapi >&- 2>&- && pwd)"
+esapi_resources="$(\cd ../../../configuration/esapi >&- 2>&- && pwd)"
+esapi_resources_test="$(\cd ../../../src/test/resources/esapi >&- 2>&- && pwd)"
 
-log4j_properties="../../../configuration/log4j.xml"
+log4j_properties="../../../src/test/resources/log4j.xml"
 
 if [[ ! -r "$esapi_resources"/ESAPI.properties ]]
-then echo 2>&1 "setenv-svn.sh: Can't read ESAPI.properties in $esapi_properties"
+then echo 2>&1 "setenv-svn.sh: Can't read ESAPI.properties in $esapi_resources"
      return 1   # Don't use 'exit' here or it will kill their current shell.
 fi
 
 if [[ ! -r "$esapi_resources_test"/ESAPI.properties ]]
-then echo 2>&1 "setenv-svn.sh: Can't read ESAPI.properties in $esapi_properties_test"
+then echo 2>&1 "setenv-svn.sh: Can't read ESAPI.properties in $esapi_resources_test"
      return 1   # Don't use 'exit' here or it will kill their current shell.
 fi
 
@@ -40,9 +40,11 @@ then echo 2>&1 "setenv-svn.sh: Can't read log4j.xml: $log4j_properties"
      return 1   # Don't use 'exit' here or it will kill their current shell.
 fi
 
-## echo "esapi_resources=$esapi_resources"
-## echo "esapi_resources_test=$esapi_resources_test"
-## echo "log4j_properties=$log4j_properties"
-## echo "esapi_classpath=$esapi_classpath"
+echo ############################################################
+echo "esapi_resources=$esapi_resources"
+echo "esapi_resources_test=$esapi_resources_test"
+echo "log4j_properties=$log4j_properties"
+echo "esapi_classpath=$esapi_classpath"
+echo ############################################################
 
 export esapi_classpath esapi_resources esapi_resources_test log4j_properties
