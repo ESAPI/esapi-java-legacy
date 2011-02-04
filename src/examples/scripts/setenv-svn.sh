a371d729696bbda53f524d9e2b900053ce735d34
kevin.w.wall <kevin.w.wall@69e6d614-4441-0410-9a8b-65af957f44db>
2011-02-04 05:16:58 +0000

    Update script to work with ESAPI 2.0_rc10 and later. Improve output.

--- src/examples/scripts/setenv-svn.sh
+++ src/examples/scripts/setenv-svn.sh
@@ -12,19 +12,20 @@
 # IMPORTANT NOTE:  Since you may have multiple (say) log4j jars under
 #                  your Maven2 repository under $HOME/.m2/respository, we
 #                  look for the specific versions that ESAPI was using as of
-#                  ESAPI 2.0_RC7 release on 2010/08/22. If these versions
+#                  ESAPI 2.0_RC10 release on 2010/10/18. If these versions
 #                  changed, they will have to be reflected here.
 #
 esapi_classpath=".:\
-$(ls ../../../target/esapi-*.jar):\
-$(./findjar.sh log4j-1.2.12.jar):\
+../../../target/classes:\
+$(ls ../../../target/esapi-*.jar 2>&- || echo .):\
+$(./findjar.sh log4j-1.2.16.jar):\
 $(./findjar.sh commons-fileupload-1.2.jar):\
 $(./findjar.sh servlet-api-2.4.jar)"
 
-esapi_resources="$(\cd ../../main/resources/.esapi >&- 2>&- && pwd)"
-esapi_resources_test="$(\cd ../../test/resources/.esapi >&- 2>&- && pwd)"
+esapi_resources="$(\cd ../../../configuration/esapi >&- 2>&- && pwd)"
+esapi_resources_test="$(\cd ../../../src/test/resources/esapi >&- 2>&- && pwd)"
 
-log4j_properties="../../main/resources/log4j.xml"
+log4j_properties="../../../src/test/resources/log4j.xml"
 
 if [[ ! -r "$esapi_resources"/ESAPI.properties ]]
 then echo 2>&1 "setenv-svn.sh: Can't read ESAPI.properties in $esapi_resources"
@@ -41,9 +42,11 @@ then echo 2>&1 "setenv-svn.sh: Can't read log4j.xml: $log4j_properties"
      return 1   # Don't use 'exit' here or it will kill their current shell.
 fi
 
-## echo "esapi_resources=$esapi_resources"
-## echo "esapi_resources_test=$esapi_resources_test"
-## echo "log4j_properties=$log4j_properties"
-## echo "esapi_classpath=$esapi_classpath"
+echo ############################################################
+echo "esapi_resources=$esapi_resources"
+echo "esapi_resources_test=$esapi_resources_test"
+echo "log4j_properties=$log4j_properties"
+echo "esapi_classpath=$esapi_classpath"
+echo ############################################################
 
 export esapi_classpath esapi_resources esapi_resources_test log4j_properties
