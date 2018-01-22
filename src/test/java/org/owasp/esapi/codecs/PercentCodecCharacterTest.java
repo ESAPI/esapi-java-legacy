c4d77b142edc14d22fcba6b6d7f8ffc7f0221394
Jeremiah Stacey <jeremiah.j.stacey@gmail.com>
2018-01-22 09:30:24 -0600

    PercentCodecCharacterTest cleanup
    Splitting up the PushbackSequence test for better readability.  Adding
    documentation.  Whitespace cleanup.

--- src/test/java/org/owasp/esapi/codecs/PercentCodecCharacterTest.java
+++ src/test/java/org/owasp/esapi/codecs/PercentCodecCharacterTest.java
@@ -11,98 +11,109 @@ import org.owasp.esapi.codecs.PercentCodec;
 import org.owasp.esapi.codecs.PushbackString;
 
 /**
- * FIXME:  Document intent of class.  General Function, purpose of creation, intended feature, etc.
- *  Why do people care this exists? 
- * @author Jeremiah
- * @since Jan 20, 2018
- *
+ *  Codec validation focused on the PercentCodec Character-based api.
+ *  
  */
 public class PercentCodecCharacterTest extends AbstractCodecCharacterTest {
     private static final char[] PERCENT_CODEC_IMMUNE;
 
     static {
         /*
-         * The percent codec contains a unique immune character set which include letters and numbers that will not be transformed.
-         * 
+         * The percent codec contains a unique immune character set which include letters and numbers that will not be
+         * transformed.
          * It is being replicated here to allow the test to reasonably expect the correct state back.
          */
         List<Character> immune = new ArrayList<>();
         // 65 - 90 (capital letters) 97 - 122 lower case 48 - 57 digits
-        //numbers
-        for (int index = 48 ; index < 58; index ++) {
-            immune.add((char)index);
+        // numbers
+        for (int index = 48; index < 58; index++) {
+            immune.add((char) index);
         }
-        //letters
-        for (int index = 65 ; index < 91; index ++) {
-            Character capsChar = (char)index;
+        // letters
+        for (int index = 65; index < 91; index++) {
+            Character capsChar = (char) index;
             immune.add(capsChar);
-            immune.add(Character.toLowerCase(capsChar));            
+            immune.add(Character.toLowerCase(capsChar));
         }
-        
+
         PERCENT_CODEC_IMMUNE = new char[immune.size()];
         for (int index = 0; index < immune.size(); index++) {
             PERCENT_CODEC_IMMUNE[index] = immune.get(index).charValue();
         }
     }
 
-    @Parameters(name="{0}")
+    @Parameters(name = "{0}")
     public static Collection<Object[]> buildTests() {
         Collection<Object[]> tests = new ArrayList<>();
-        
+
         Collection<CodecCharacterTestTuple> tuples = new ArrayList<>();
-        tuples.add(newTuple("%3C",Character.valueOf('<')));
-      
-        tuples.add(newTuple("%C4%80",Character.valueOf((char)0x100)));
-        tuples.add(newTuple("%00",Character.MIN_VALUE));
-        tuples.add(newTuple("%3D",'='));
-        tuples.add(newTuple("%26",'&'));
-        
+        tuples.add(newTuple("%3C", Character.valueOf('<')));
+
+        tuples.add(newTuple("%C4%80", Character.valueOf((char) 0x100)));
+        tuples.add(newTuple("%00", Character.MIN_VALUE));
+        tuples.add(newTuple("%3D", '='));
+        tuples.add(newTuple("%26", '&'));
+
         for (char c : PERCENT_CODEC_IMMUNE) {
             tuples.add(newTuple(Character.toString(c), c));
         }
-        
+
         for (CodecCharacterTestTuple tuple : tuples) {
-            tests.add(new Object[]{tuple});
+            tests.add(new Object[] { tuple });
         }
-        
+
         return tests;
     }
-    
-    
-    
+
     private static CodecCharacterTestTuple newTuple(String encodedInput, Character decoded) {
         CodecCharacterTestTuple tuple = new CodecCharacterTestTuple();
         tuple.codec = new PercentCodec();
         tuple.encodeImmune = PERCENT_CODEC_IMMUNE;
         tuple.decodedValue = decoded;
         tuple.input = encodedInput;
-        
+
         return tuple;
     }
-    /**
-     * @param tuple
-     */
+
     public PercentCodecCharacterTest(CodecCharacterTestTuple tuple) {
         super(tuple);
     }
-    
-    
+
     @Override
     @Test
     public void testDecodePushbackSequence() {
-        //If the input is not decoded, then null should be returned and the pushback string index should be unchanged.
-        //If the input is decode then the decoded value should be returned, and the pushback string index should have progressed forward.
-        
+        // check duplicated from PushbackSequence handling in PercentCodec.
+        boolean inputIsEncoded = input.startsWith("%");
+
+        if (inputIsEncoded) {
+            assertInputIsDecodedToValue();
+        } else {
+            assertInputIsDecodedToNull();
+        }
+    }
+
+    /**
+     * tests that when Input is decoded through a PushbackString that the decodedValue reference is returned and that
+     * the PushbackString index has incremented.
+     */
+    @SuppressWarnings("unchecked")
+    private void assertInputIsDecodedToValue() {
         PushbackString pbs = new PushbackString(input);
         int startIndex = pbs.index();
-        boolean shouldDecode = input.startsWith("%");
-        if (shouldDecode) {
-            Assert.assertEquals(decodedValue, codec.decodeCharacter(pbs));
-            Assert.assertTrue(startIndex < pbs.index());
-        } else {
-            Assert.assertEquals(null, codec.decodeCharacter(pbs));
-            Assert.assertEquals(startIndex, pbs.index());
-        }
+        Assert.assertEquals(decodedValue, codec.decodeCharacter(pbs));
+        Assert.assertTrue(startIndex < pbs.index());
+    }
+
+    /**
+     * tests that when Input is decoded through a PushbackString that null is returned and that the PushbackString index
+     * remains unchanged.
+     */
+    @SuppressWarnings("unchecked")
+    private void assertInputIsDecodedToNull() {
+        PushbackString pbs = new PushbackString(input);
+        int startIndex = pbs.index();
+        Assert.assertEquals(null, codec.decodeCharacter(pbs));
+        Assert.assertEquals(startIndex, pbs.index());
     }
 
 }
