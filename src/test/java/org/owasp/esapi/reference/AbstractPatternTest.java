c3870f660439e39a22a14ca013c8da17aa59eb66
Jeremiah Stacey <jeremiah.j.stacey@gmail.com>
2018-01-22 08:47:34 -0600

    Pattern Validation Test Cleanup
    Adding documentation to the AbstractPatternTest to help with usability.
    Extracting message constant to a class var in
    EsapiWhitelistValidaitonPatternTests for improved readability.

--- src/test/java/org/owasp/esapi/reference/AbstractPatternTest.java
+++ src/test/java/org/owasp/esapi/reference/AbstractPatternTest.java
@@ -7,43 +7,50 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 
-
 /**
- * FIXME:  Document intent of class.  General Function, purpose of creation, intended feature, etc.
- *  Why do people care this exists? 
- * @author Jeremiah
- * @since Jan 20, 2018
- *
+ * Abstract parameterized test case meant to assist with verifying regular expressions in test scope.
+ * <br/>
+ * Sub-classes are expected to provide instances of {@link PatternTestTuple} to this instance.
+ * <br/>
+ * For better test naming output specify {@link PatternTestTuple#description} and use {@code} @Parameters (name="{0}")},
+ * where '0' is the index that the PatternTestTuple reference appears in the constructor.
  */
-@RunWith (Parameterized.class)
+@RunWith(Parameterized.class)
 public abstract class AbstractPatternTest {
-    
+
+    /**
+     * Test tuple for Pattern validation.
+     */
     protected static class PatternTestTuple {
+        /** String value to be tested against the compiled regex reference. */
         String input;
+        /** Regular expression string that will be compiled and be passed the input. */
         String regex;
+        /** Test Expectation whether input should match the compiled regex. */
         boolean shouldMatch;
+        /** Optional field to override the toString value of this tuple. */
         String description;
-        /** {@inheritDoc}*/        
+
+        /** {@inheritDoc} */
         @Override
         public String toString() {
-           return description != null ? description : regex;
+            return description != null ? description : regex;
         }
     }
 
     private String input;
     private Pattern pattern;
     private boolean shouldMatch;
-    
-    
+
     public AbstractPatternTest(PatternTestTuple tuple) {
         this.input = tuple.input;
         this.pattern = Pattern.compile(tuple.regex);
         this.shouldMatch = tuple.shouldMatch;
     }
-    
+
     @Test
     public void checkPatternMatches() {
         Assert.assertEquals(shouldMatch, pattern.matcher(input).matches());
     }
-    
+
 }
