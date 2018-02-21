bf91374d3acb5cf79943ac9127a7fd39473af881
Jeremiah Stacey <jeremiah.j.stacey@gmail.com>
2018-02-21 17:47:00 -0600

    Adding License Information
    Adding license content to files created in this effort.

--- src/test/java/org/owasp/esapi/reference/regex/AbstractPatternTest.java
+++ src/test/java/org/owasp/esapi/reference/regex/AbstractPatternTest.java
@@ -1,3 +1,16 @@
+/**
+ * OWASP Enterprise Security API (ESAPI)
+ *
+ * This file is part of the Open Web Application Security Project (OWASP)
+ * Enterprise Security API (ESAPI) project. For details, please see
+ * <a href="http://www.owasp.org/index.php/ESAPI">http://www.owasp.org/index.php/ESAPI</a>.
+ *
+ * Copyright (c) 2008-2018 - The OWASP Foundation
+ *
+ * The ESAPI is published by OWASP under the BSD license. You should read and accept the
+ * LICENSE before you use, modify, and/or redistribute this software.
+ *
+ */
 package org.owasp.esapi.reference.regex;
 
 import java.util.regex.Pattern;
