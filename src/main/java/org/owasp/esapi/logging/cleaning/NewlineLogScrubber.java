0dda8ded7d89e0dd077f0d7ca47010db3fe5679a
Jeremiah Stacey <jeremiah.j.stacey@gmail.com>
2018-08-06 04:47:14 -0500

    SLF4J New File Licenses
    Adding license content to new files.

--- src/main/java/org/owasp/esapi/logging/cleaning/NewlineLogScrubber.java
+++ src/main/java/org/owasp/esapi/logging/cleaning/NewlineLogScrubber.java
@@ -1,3 +1,17 @@
+/**
+ * OWASP Enterprise Security API (ESAPI)
+ * 
+ * This file is part of the Open Web Application Security Project (OWASP)
+ * Enterprise Security API (ESAPI) project. For details, please see
+ * <a href="http://www.owasp.org/index.php/ESAPI">http://www.owasp.org/index.php/ESAPI</a>.
+ *
+ * Copyright (c) 2007 - The OWASP Foundation
+ * 
+ * The ESAPI is published by OWASP under the BSD license. You should read and accept the
+ * LICENSE before you use, modify, and/or redistribute this software.
+ * 
+ * @created 2018
+ */
 package org.owasp.esapi.logging.cleaning;
 
 public class NewlineLogScrubber  implements LogScrubber {
