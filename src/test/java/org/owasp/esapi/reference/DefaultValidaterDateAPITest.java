ae7a93dc43928b99e76c17bff6195216f39b542a
Jeremiah Stacey <jeremiah.j.stacey@gmail.com>
2019-01-11 06:55:27 -0600

    DefaultValidator Date API Negative Tests
    Verifying workflow and response from the DateAPI when the
    DateValidationRule would result in a ValidationException being thrown.

--- src/test/java/org/owasp/esapi/reference/DefaultValidaterDateAPITest.java
+++ src/test/java/org/owasp/esapi/reference/DefaultValidaterDateAPITest.java
@@ -2,17 +2,21 @@ package org.owasp.esapi.reference;
 
 
 import java.text.DateFormat;
+import java.util.Arrays;
 import java.util.Date;
 import java.util.Locale;
 
+import org.hamcrest.core.Is;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 import org.junit.rules.TestName;
 import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.ArgumentMatchers;
 import org.mockito.Mockito;
 import org.owasp.esapi.Encoder;
@@ -38,6 +42,7 @@ public class DefaultValidaterDateAPITest {
     public TestName testName = new TestName();
     private String dateString="Input Does not matter in this context";
     
+    private ValidationException validationEx;
     private Date testDate = new Date();
     private String contextStr;
     private Encoder mockEncoder;
@@ -50,21 +55,24 @@ public class DefaultValidaterDateAPITest {
     
     @Before
     public void setup() throws Exception {
-        contextStr = testName.getMethodName();        
+        contextStr = testName.getMethodName();
+        
+        validationEx = new ValidationException(contextStr, contextStr);
         
         mockEncoder = Mockito.mock(Encoder.class);
         uit = new DefaultValidator(mockEncoder);
         
         spyDateRule = new DateValidationRule(contextStr, mockEncoder, testFormat);
         spyDateRule = Mockito.spy(spyDateRule);
-        
-        
         PowerMockito.whenNew(DateValidationRule.class).withArguments(ArgumentMatchers.anyString(), ArgumentMatchers.eq(mockEncoder), ArgumentMatchers.eq(testFormat)).thenReturn(spyDateRule);
+        
+        errors = Mockito.spy(errors);
+        PowerMockito.whenNew(ValidationErrorList.class).withNoArguments().thenReturn(errors);
     }
     
     @After
     public void tearDown() {
-        Mockito.verifyNoMoreInteractions(spyDateRule);
+        Mockito.verifyNoMoreInteractions(spyDateRule, errors);
     }
     
     @Test
@@ -74,6 +82,8 @@ public class DefaultValidaterDateAPITest {
         boolean isValid = uit.isValidDate(contextStr, dateString, testFormat, true);
         Assert.assertTrue("Mock is configured to return a valid date, return should be equally valid.", isValid);
         
+        Mockito.verify(errors, Mockito.atLeastOnce()).isEmpty();
+        Mockito.verify(errors, Mockito.times(0)).errors();
         Mockito.verify(spyDateRule, Mockito.times(1)).setAllowNull(true);
         Mockito.verify(spyDateRule, Mockito.times(1)).sanitize(ArgumentMatchers.eq(contextStr), ArgumentMatchers.eq(dateString), ArgumentMatchers.isA(ValidationErrorList.class));
     }
@@ -85,6 +95,8 @@ public class DefaultValidaterDateAPITest {
         boolean isValid = uit.isValidDate(contextStr, dateString, testFormat, true, errors);
         Assert.assertTrue("Mock is configured to return a valid date, return should be equally valid.", isValid);
         
+        Mockito.verify(errors, Mockito.atLeastOnce()).isEmpty();
+        Mockito.verify(errors, Mockito.times(0)).errors();
         Mockito.verify(spyDateRule, Mockito.times(1)).setAllowNull(true);
         Mockito.verify(spyDateRule, Mockito.times(1)).sanitize(ArgumentMatchers.eq(contextStr), ArgumentMatchers.eq(dateString), ArgumentMatchers.eq(errors));
     }
@@ -95,6 +107,8 @@ public class DefaultValidaterDateAPITest {
         Date validDate = uit.getValidDate(contextStr, dateString, testFormat, true);
         Assert.assertEquals("ValidDate should match the mock's configured return value", testDate, validDate);
         
+        Mockito.verify(errors, Mockito.atLeastOnce()).isEmpty();
+        Mockito.verify(errors, Mockito.times(0)).errors();
         Mockito.verify(spyDateRule, Mockito.times(1)).setAllowNull(true);
         Mockito.verify(spyDateRule, Mockito.times(1)).sanitize(ArgumentMatchers.eq(contextStr), ArgumentMatchers.eq(dateString), ArgumentMatchers.isA(ValidationErrorList.class));
     }
@@ -104,7 +118,74 @@ public class DefaultValidaterDateAPITest {
         Mockito.doReturn(testDate).when(spyDateRule).sanitize(ArgumentMatchers.eq(contextStr), ArgumentMatchers.eq(dateString), ArgumentMatchers.isA(ValidationErrorList.class));
         Date validDate = uit.getValidDate(contextStr, dateString, testFormat, true, errors);
         Assert.assertEquals("ValidDate should match the mock's configured return value", testDate, validDate);
+        
+        Mockito.verify(errors, Mockito.atLeastOnce()).isEmpty();
+        Mockito.verify(errors, Mockito.times(0)).errors();
         Mockito.verify(spyDateRule, Mockito.times(1)).setAllowNull(true);
         Mockito.verify(spyDateRule, Mockito.times(1)).sanitize(ArgumentMatchers.eq(contextStr), ArgumentMatchers.eq(dateString), ArgumentMatchers.eq(errors));
     }
+    
+    
+    @Test
+    public void testIsValidDateOnValidationError() {
+        Mockito.doReturn(false).when(errors).isEmpty();
+        Mockito.doReturn(Arrays.asList(validationEx)).when(errors).errors();
+        
+        Mockito.doReturn(testDate).when(spyDateRule).sanitize(ArgumentMatchers.eq(contextStr), ArgumentMatchers.eq(dateString), ArgumentMatchers.isA(ValidationErrorList.class));
+        
+        boolean isValid = uit.isValidDate(contextStr, dateString, testFormat, true);
+        Assert.assertFalse("On ValidationException input should be invalid", isValid);
+        
+        Mockito.verify(errors, Mockito.atLeastOnce()).isEmpty();
+        Mockito.verify(errors, Mockito.times(1)).errors();
+        Mockito.verify(spyDateRule, Mockito.times(1)).setAllowNull(true);
+        Mockito.verify(spyDateRule, Mockito.times(1)).sanitize(ArgumentMatchers.eq(contextStr), ArgumentMatchers.eq(dateString), ArgumentMatchers.isA(ValidationErrorList.class));
+    }
+    
+    @Test
+    public void testIsValidDateErrorListOnValidationError() {
+        Mockito.doReturn(false).when(errors).isEmpty();
+        Mockito.doReturn(Arrays.asList(validationEx)).when(errors).errors();
+        
+        Mockito.doReturn(testDate).when(spyDateRule).sanitize(ArgumentMatchers.eq(contextStr), ArgumentMatchers.eq(dateString), ArgumentMatchers.isA(ValidationErrorList.class));
+        
+        boolean isValid = uit.isValidDate(contextStr, dateString, testFormat, true, errors);
+        Assert.assertFalse("On ValidationException input should be invalid", isValid);
+        
+        Mockito.verify(errors, Mockito.times(2)).isEmpty();
+        Mockito.verify(errors, Mockito.times(0)).errors();
+        Mockito.verify(spyDateRule, Mockito.times(1)).setAllowNull(true);
+        Mockito.verify(spyDateRule, Mockito.times(1)).sanitize(ArgumentMatchers.eq(contextStr), ArgumentMatchers.eq(dateString), ArgumentMatchers.eq(errors));
+    }
+    @Test
+    public void testGetValidDateOnValidationError() throws IntrusionException, ValidationException {
+        exEx.expect(Is.is(validationEx));
+        Mockito.doReturn(false).when(errors).isEmpty();
+        Mockito.doReturn(Arrays.asList(validationEx)).when(errors).errors();
+
+        Mockito.doReturn(testDate).when(spyDateRule).sanitize(ArgumentMatchers.eq(contextStr), ArgumentMatchers.eq(dateString), ArgumentMatchers.isA(ValidationErrorList.class));
+        try {
+            uit.getValidDate(contextStr, dateString, testFormat, true);
+        } finally {
+            Mockito.verify(errors, Mockito.atLeastOnce()).isEmpty();
+            Mockito.verify(errors, Mockito.times(1)).errors();
+            Mockito.verify(spyDateRule, Mockito.times(1)).setAllowNull(true);
+            Mockito.verify(spyDateRule, Mockito.times(1)).sanitize(ArgumentMatchers.eq(contextStr), ArgumentMatchers.eq(dateString), ArgumentMatchers.isA(ValidationErrorList.class));
+        }
+    }
+    
+    @Test
+    public void testGetValidDateErrorListOnValidationError() {
+        Mockito.doReturn(false).when(errors).isEmpty();
+        Mockito.doReturn(Arrays.asList(validationEx)).when(errors).errors();
+
+        Mockito.doReturn(testDate).when(spyDateRule).sanitize(ArgumentMatchers.eq(contextStr), ArgumentMatchers.eq(dateString), ArgumentMatchers.isA(ValidationErrorList.class));
+        Date validDate = uit.getValidDate(contextStr, dateString, testFormat, true, errors);
+        Assert.assertNull(validDate);
+        Mockito.verify(errors, Mockito.atLeastOnce()).isEmpty();
+        Mockito.verify(errors, Mockito.times(0)).errors();
+        Mockito.verify(spyDateRule, Mockito.times(1)).setAllowNull(true);
+        Mockito.verify(spyDateRule, Mockito.times(1)).sanitize(ArgumentMatchers.eq(contextStr), ArgumentMatchers.eq(dateString), ArgumentMatchers.isA(ValidationErrorList.class));
+    }
+
 }
