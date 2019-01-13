695cf4f25482cc4acd3b5c1d5600d6e8893f34b5
Jeremiah Stacey <jeremiah.j.stacey@gmail.com>
2019-01-13 12:44:29 -0600

    JavaLogFactory Concurrency Test
    Test case showing that in multi-threaded requests for a single reference,
    multiple Logger references may be created.

---
+++ src/test/java/org/owasp/esapi/reference/JavaLogFactoryTest.java
@@ -0,0 +1,56 @@
+package org.owasp.esapi.reference;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.CountDownLatch;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.owasp.esapi.Logger;
+
+
+public class JavaLogFactoryTest {
+
+    @Test
+    public void testConcurrentLogRequest() throws InterruptedException {
+        final ConcurrentHashMap<Integer, Logger> logCapture = new ConcurrentHashMap<>();
+        List<Thread> threads = new ArrayList<>();
+        final CountDownLatch forceConcurrency = new CountDownLatch(1);
+        for (int x = 0 ; x < 10; x ++) {
+            final int requestIndex = x;
+            Runnable requestLog = new Runnable() {
+                @Override
+                public void run() {
+                   try {
+                    forceConcurrency.await();
+                } catch (InterruptedException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+                   logCapture.put(requestIndex, JavaLogFactory.getInstance().getLogger(JavaLogFactoryTest.class));
+                }
+            };
+            
+            threads.add(new Thread(requestLog, "Request Log " + x));
+        }
+        
+        for (Thread thread : threads) {
+            thread.start();
+        }
+        
+        forceConcurrency.countDown();
+        
+        for (Thread thread: threads) {
+            thread.join();
+        }
+        
+        
+        Set<Logger> uniqueLoggers = new HashSet<>(logCapture.values());
+        
+        Assert.assertEquals(1, uniqueLoggers.size());
+        
+    }
+}
