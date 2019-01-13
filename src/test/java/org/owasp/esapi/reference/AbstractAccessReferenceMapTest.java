c7ab789cd7c53dbb656c5a6f1ec8bb4d8b85ca7f
Jeremiah Stacey <jeremiah.j.stacey@gmail.com>
2019-01-13 06:48:16 -0600

    AbstractAccessReferenceMapTest Extension
    Adding method which shows that it is possible to create a corrupted direct
    -> Indirect to Indirect -> Direct Object relationship when using the
    public update(Set) API.

--- src/test/java/org/owasp/esapi/reference/AbstractAccessReferenceMapTest.java
+++ src/test/java/org/owasp/esapi/reference/AbstractAccessReferenceMapTest.java
@@ -1,7 +1,10 @@
 package org.owasp.esapi.reference;
 
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.mockito.Mockito;
 import org.powermock.reflect.Whitebox;
@@ -61,5 +64,37 @@ public class AbstractAccessReferenceMapTest {
         lockIndirectRefs.join();
 
         Mockito.verify(map,Mockito.times(1)).getUniqueReference();
+    }
+    
+    @Test
+    public void verifyNoDuplicateKeysOnUpdateReplace() {
+        @SuppressWarnings("unchecked")
+        final AbstractAccessReferenceMap<Object> map = Mockito.mock(AbstractAccessReferenceMap.class, Mockito.withSettings().useConstructor()
+                .defaultAnswer(Mockito.CALLS_REAL_METHODS)
+                );
+        Object indirectObj1 = new Object();
+        Object indirectObj2 = new Object();
+        Mockito.when(map.getUniqueReference()).thenReturn(indirectObj1); 
+        
+        Object direct1 = new Object();
+        Object direct2 = new Object();
+        
+        map.addDirectReference(direct1);
+        
+        Mockito.reset(map);
+        
+        Set<Object> newDirectElements = new HashSet<>();
+        newDirectElements.add(direct2);
+        newDirectElements.add(direct1);
+        
+        Mockito.when(map.getUniqueReference()).thenReturn(indirectObj1).thenReturn(indirectObj2); 
+        
+        map.update(newDirectElements);
+        
+        //Needs to be called 2 times to get past the first duplicate key. This verifies that we're inserting unique pairs.
+        Mockito.verify(map, Mockito.times(2)).getUniqueReference();
+        
+        Assert.assertEquals(indirectObj1, map.getIndirectReference(direct1));
+        Assert.assertEquals(indirectObj2, map.getIndirectReference(direct2));
     }
 }
