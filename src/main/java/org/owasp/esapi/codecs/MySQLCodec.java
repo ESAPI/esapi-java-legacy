1098fc6841d4ffa81c3111948a8cdf19f1ddeca7
Jeremiah Stacey <jeremiah.j.stacey@gmail.com>
2019-01-20 05:37:53 -0600

    MySQLCodec Documentation Updates
    Updating class documentation to clarify the intended support provided by
    the implementation.

--- src/main/java/org/owasp/esapi/codecs/MySQLCodec.java
+++ src/main/java/org/owasp/esapi/codecs/MySQLCodec.java
@@ -18,14 +18,47 @@ package org.owasp.esapi.codecs;
 
 
 /**
- * Implementation of the Codec interface for MySQL strings. See https://www.owasp.org/index.php/SQL_Injection_Prevention_Cheat_Sheet#MySQL_Escaping
- * for more information.
+ * Codec implementation which can be used to escape string literals in MySQL.
+ * </br>
+ * Implementation accepts 2 Modes as identified by the OWASP Recommended
+ * escaping strategies:
+ * <ul>
+ * <li><b>ANSI</b> <br>
+ * Simply encode all ' (single tick) characters with '' (two single ticks)</li>
+ * <br>
+ * <li><b>Standard</b>
  * 
+ * <pre>
+ *   NUL (0x00) --> \0  [This is a zero, not the letter O]
+ *   BS  (0x08) --> \b
+ *   TAB (0x09) --> \t
+ *   LF  (0x0a) --> \n
+ *   CR  (0x0d) --> \r
+ *   SUB (0x1a) --> \Z
+ *   "   (0x22) --> \"
+ *   %   (0x25) --> \%
+ *   '   (0x27) --> \'
+ *   \   (0x5c) --> \\
+ *   _   (0x5f) --> \_ 
+ *   <br>
+ *   all other non-alphanumeric characters with ASCII values less than 256  --> \c
+ *   where 'c' is the original non-alphanumeric character.
+ * </pre>
  * 
- * @author Jeff Williams (jeff.williams .at. aspectsecurity.com) <a
- *         href="http://www.aspectsecurity.com">Aspect Security</a>
+ * </li>
+ * 
+ * </ul>
+ * 
+ * @author Jeff Williams (jeff.williams .at. aspectsecurity.com)
+ *         <a href="http://www.aspectsecurity.com">Aspect Security</a>
  * @since June 1, 2007
- * @see org.owasp.esapi.Encoder
+ * 
+ * @see <a href=
+ *      "https://dev.mysql.com/doc/refman/8.0/en/string-literals.html">MySQL 8.0
+ *      String Literals</a>
+ * @see <a href=
+ *      "https://www.owasp.org/index.php/SQL_Injection_Prevention_Cheat_Sheet#MySQL_Escaping">OWASP
+ *      SQL_Injection_Prevention_Cheat_Sheet#MySQL_Escaping</a>
  */
 public class MySQLCodec extends AbstractCharacterCodec {
     /**
@@ -54,7 +87,7 @@ public class MySQLCodec extends AbstractCharacterCodec {
 
     /** Target MySQL Server is running in Standard MySQL (Default) mode. */
     public static final int MYSQL_MODE = 0;
-    /** Target MySQL Server is running in {@link "http://dev.mysql.com/doc/refman/5.0/en/ansi-mode.html"} ANSI Mode */
+    /** Target MySQL Server is running in ANSI Mode */
     public static final int ANSI_MODE = 1;
 	
 	//private int mode = 0;
