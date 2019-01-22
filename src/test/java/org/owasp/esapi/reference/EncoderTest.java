3556aa48ff6ab30a9ca3ccfb138f2e845546c423
Jeremiah Stacey <jeremiah.j.stacey@gmail.com>
2019-01-21 19:01:47 -0600

    Removing testMySQLANSIModeQuoteInjection
    From research, on issue #31 double quotes within the single quote literals
    supported in ANSI_QUOTE modes do not require explicit escape handling.

--- src/test/java/org/owasp/esapi/reference/EncoderTest.java
+++ src/test/java/org/owasp/esapi/reference/EncoderTest.java
@@ -458,13 +458,6 @@ public class EncoderTest extends TestCase {
         assertEquals("Oracle", null, instance.encodeForSQL(oracle, null));
         assertEquals("Oracle", "Jeff'' or ''1''=''1", instance.encodeForSQL(oracle, "Jeff' or '1'='1"));
     }
-
-    public void testMySQLANSIModeQuoteInjection() {
-        Encoder instance = ESAPI.encoder();
-        Codec c = new MySQLCodec(MySQLCodec.Mode.ANSI);
-        assertEquals("MySQL Ansi Quote Injection Bug", " or 1=1 -- -", instance.encodeForSQL(c, "\" or 1=1 -- -"));
-    }
-
     
     /**
 	 * Test of encodeForLDAP method, of class org.owasp.esapi.Encoder.
