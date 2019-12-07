3a696dcb3797a1cafa62d991373c4c69e198e062
Jeremiah Stacey <jeremiah.j.stacey@gmail.com>
2019-12-07 05:30:43 -0600

    EventTypeLogSupplier Documentation
    Updating javadoc

--- src/main/java/org/owasp/esapi/logging/appender/EventTypeLogSupplier.java
+++ src/main/java/org/owasp/esapi/logging/appender/EventTypeLogSupplier.java
@@ -4,10 +4,20 @@ import java.util.function.Supplier;
 
 import org.owasp.esapi.Logger.EventType;
 
+/**
+ * Supplier implementation which returns a consistent String representation of
+ * an EventType for logging
+ *
+ */
 public class EventTypeLogSupplier implements Supplier<String> {
-
+	/** EventType reference to supply log representation of. */
 	private final EventType eventType;
-	
+
+	/**
+	 * Ctr
+	 * 
+	 * @param evtyp EventType reference to supply log representation for
+	 */
 	public EventTypeLogSupplier(EventType evtyp) {
 		this.eventType = evtyp;
 	}
@@ -16,5 +26,4 @@ public class EventTypeLogSupplier implements Supplier<String> {
 	public String get() {
 		return eventType.toString();
 	}
-
 }
