3def7fb99efa33f36a098a67d85542cb78ee52b3
Jeremiah Stacey <jeremiah.j.stacey@gmail.com>
2019-12-07 06:06:02 -0600

    LogPrefixAppender Documentation
    Updating Javadoc

--- src/main/java/org/owasp/esapi/logging/appender/LogPrefixAppender.java
+++ src/main/java/org/owasp/esapi/logging/appender/LogPrefixAppender.java
@@ -2,28 +2,45 @@ package org.owasp.esapi.logging.appender;
 
 import org.owasp.esapi.Logger.EventType;
 
+/**
+ * LogAppender Implementation which can prefix the common logger information for
+ * EventType, Client data, and server data.
+ */
 public class LogPrefixAppender implements LogAppender {
-	private final String RESULT_FORMAT="[%s %s -> %s] %s";
-	
+	/** Output format used to assemble return values. */
+	private static final String RESULT_FORMAT = "[%s %s -> %s] %s";// EVENT_TYPE, CLIENT_INFO, SERVER_INFO, messageBody
+
+	/** Whether or not to record client information. */
 	private final boolean logClientInfo;
+	/** Whether or not to record server ip information. */
 	private final boolean logServerIp;
+	/** Whether or not to record application name. */
 	private final boolean logApplicationName;
+	/** Application Name to record. */
 	private final String appName;
-	
+
+	/**
+	 * Ctr.
+	 * 
+	 * @param logClientInfo      Whether or not to record client information
+	 * @param logServerIp        Whether or not to record server ip information
+	 * @param logApplicationName Whether or not to record application name
+	 * @param appName            Application Name to record.
+	 */
 	public LogPrefixAppender(boolean logClientInfo, boolean logServerIp, boolean logApplicationName, String appName) {
 		this.logClientInfo = logClientInfo;
 		this.logServerIp = logServerIp;
 		this.logApplicationName = logApplicationName;
 		this.appName = appName;
 	}
-	
+
 	@Override
 	public String appendTo(String logName, EventType eventType, String message) {
 		EventTypeLogSupplier eventTypeSupplier = new EventTypeLogSupplier(eventType);
-		
+
 		ClientInfoSupplier clientInfoSupplier = new ClientInfoSupplier();
 		clientInfoSupplier.setLogUserInfo(logClientInfo);
-		
+
 		ServerInfoSupplier serverInfoSupplier = new ServerInfoSupplier(logName);
 		serverInfoSupplier.setLogServerIp(logServerIp);
 		serverInfoSupplier.setLogApplicationName(logApplicationName, appName);
@@ -31,7 +48,7 @@ public class LogPrefixAppender implements LogAppender {
 		String eventTypeMsg = eventTypeSupplier.get();
 		String clientInfoMsg = clientInfoSupplier.get();
 		String serverInfoMsg = serverInfoSupplier.get();
-		
-		return String.format(RESULT_FORMAT, eventTypeMsg, clientInfoMsg,serverInfoMsg, message);
+
+		return String.format(RESULT_FORMAT, eventTypeMsg, clientInfoMsg, serverInfoMsg, message);
 	}
 }
