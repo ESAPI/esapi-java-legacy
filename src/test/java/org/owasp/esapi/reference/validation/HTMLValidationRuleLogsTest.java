7450ff7cbde90df46c0056608a90d4310b128bce
kwwall <kevin.w.wall@gmail.com>
2020-01-21 00:55:46 -0500

    New test files for GitHub issue #521; initial commit.

---
+++ src/test/java/org/owasp/esapi/reference/validation/HTMLValidationRuleLogsTest.java
@@ -0,0 +1,158 @@
+/**
+ * OWASP Enterprise Security API (ESAPI)
+ *
+ * This file is part of the Open Web Application Security Project (OWASP)
+ * Enterprise Security API (ESAPI) project. For details, please see
+ * <a href="http://www.owasp.org/index.php/ESAPI">http://www.owasp.org/index.php/ESAPI</a>.
+ *
+ * Copyright (c) 2019 - The OWASP Foundation
+ *
+ * The ESAPI is published by OWASP under the BSD license. You should read and accept the
+ * LICENSE before you use, modify, and/or redistribute this software.
+ *
+ * @author kevin.w.wall@gmail.com
+ * @since 2019
+ */
+package org.owasp.esapi.reference;
+
+import org.owasp.esapi.ESAPI;
+import org.owasp.esapi.EncoderConstants;
+import org.owasp.esapi.SecurityConfiguration;
+import org.owasp.esapi.SecurityConfigurationWrapper;
+import org.owasp.esapi.ValidationErrorList;
+import org.owasp.esapi.ValidationRule;
+import org.owasp.esapi.Validator;
+import org.owasp.esapi.errors.ValidationException;
+import org.owasp.esapi.filters.SecurityWrapperRequest;
+import org.owasp.esapi.reference.validation.HTMLValidationRule;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * The Class HTMLValidationRuleLogsTest.
+ *
+ * Based on original test cases, testGetValidSafeHTML() and
+ * testIsValidSafeHTML() from ValidatorTest by
+ *      Mike Fauzy (mike.fauzy@aspectsecurity.com) and
+ *      Jeff Williams (jeff.williams@aspectsecurity.com)
+ * that were originally part of src/test/java/org/owasp/esapi/reference/ValidatorTest.java.
+ *
+ * This class tests the cases where the new ESAPI.property
+ *      Validator.ValidationRule.getValid.ignore509Fix
+ * is set to 'true', which causes certain calls to
+ * ESAPI.validator().getValidSafeHTML() or ESAPI.validator().isValidSafeHTML()
+ * to simply log a warning and return the cleansed (sanitizied) output rather
+ * than throwing a ValidationException when certain unsafe input is
+ * encountered.
+ */
+public class HTMLValidationRuleLogsTest extends TestCase {
+	// private SecurityConfiguration origConfig;
+
+	private static class ConfOverride extends SecurityConfigurationWrapper {
+        private boolean desiredReturn = true;
+
+		ConfOverride(SecurityConfiguration orig, boolean desiredReturn) {
+			super(orig);
+            this.desiredReturn = desiredReturn;
+		}
+
+		@Override
+		public Boolean getBooleanProp(String propName) {
+            // Would it be better making this file a static import?
+			if ( propName.equals( org.owasp.esapi.reference.DefaultSecurityConfiguration.VALIDATOR_IGNORE509 ) ) {
+                return desiredReturn;
+            } else {
+                return super.getBooleanProp( propName );
+            }
+        }
+    }
+
+    public static Test suite() {
+        return new TestSuite(HTMLValidationRuleLogsTest.class);
+    }
+
+    /**
+     * Instantiates a new HTTP utilities test.
+     *
+     * @param testName the test name
+     */
+    public HTMLValidationRuleLogsTest(String testName) {
+        super(testName);
+    }
+
+    @Override
+    protected void tearDown() throws Exception {
+        ESAPI.override(null);
+    }
+
+	@Override
+    protected void setUp() throws Exception {
+		ESAPI.override(
+			new ConfOverride( ESAPI.securityConfiguration(), true )
+		);
+
+    }
+
+    public void testGetValidSafeHTML() throws Exception {
+        System.out.println("getValidSafeHTML");
+        Validator instance = ESAPI.validator();
+        ValidationErrorList errors = new ValidationErrorList();
+
+        HTMLValidationRule rule = new HTMLValidationRule("test");
+        ESAPI.validator().addRule(rule);
+
+        assertEquals("Test.", ESAPI.validator().getRule("test").getValid("test", "Test. <script>alert(document.cookie)</script>"));
+
+        String test1 = "<b>Jeff</b>";
+        String result1 = instance.getValidSafeHTML("test", test1, 100, false, errors);
+        assertEquals(test1, result1);
+
+        String test2 = "<a href=\"http://www.aspectsecurity.com\">Aspect Security</a>";
+        String result2 = instance.getValidSafeHTML("test", test2, 100, false, errors);
+        assertEquals(test2, result2);
+
+        String test3 = "Test. <script>alert(document.cookie)</script>";
+        assertEquals("Test.", rule.getSafe("test", test3));
+
+        assertEquals("Test. &lt;<div>load=alert()</div>", rule.getSafe("test", "Test. <<div on<script></script>load=alert()"));
+        assertEquals("Test. <div>b</div>", rule.getSafe("test", "Test. <div style={xss:expression(xss)}>b</div>"));
+        assertEquals("Test. alert(document.cookie)", rule.getSafe("test", "Test. <s%00cript>alert(document.cookie)</script>"));
+        assertEquals("Test. alert(document.cookie)", rule.getSafe("test", "Test. <s\tcript>alert(document.cookie)</script>"));
+        assertEquals("Test. alert(document.cookie)", rule.getSafe("test", "Test. <s\tcript>alert(document.cookie)</script>"));
+        // TODO: ENHANCE waiting for a way to validate text headed for an attribute for scripts
+        // This would be nice to catch, but just looks like text to AntiSamy
+        // assertFalse(instance.isValidSafeHTML("test", "\" onload=\"alert(document.cookie)\" "));
+        // String result4 = instance.getValidSafeHTML("test", test4);
+        // assertEquals("", result4);
+    }
+
+
+    public void testIsValidSafeHTML() {
+        System.out.println("isValidSafeHTML");
+        Validator instance = ESAPI.validator();
+
+        assertTrue(instance.isValidSafeHTML("test", "<b>Jeff</b>", 100, false));
+        assertTrue(instance.isValidSafeHTML("test", "<a href=\"http://www.aspectsecurity.com\">Aspect Security</a>", 100, false));
+        assertTrue(instance.isValidSafeHTML("test", "Test. <script>alert(document.cookie)</script>", 100, false));
+        assertTrue(instance.isValidSafeHTML("test", "Test. <div style={xss:expression(xss)}>", 100, false));
+        assertTrue(instance.isValidSafeHTML("test", "Test. <s%00cript>alert(document.cookie)</script>", 100, false));
+        assertTrue(instance.isValidSafeHTML("test", "Test. <s\tcript>alert(document.cookie)</script>", 100, false));
+        assertTrue(instance.isValidSafeHTML("test", "Test. <s\r\n\0cript>alert(document.cookie)</script>", 100, false));
+
+        // TODO: waiting for a way to validate text headed for an attribute for scripts
+        // This would be nice to catch, but just looks like text to AntiSamy
+        // assertFalse(instance.isValidSafeHTML("test", "\" onload=\"alert(document.cookie)\" "));
+        ValidationErrorList errors = new ValidationErrorList();
+        assertTrue(instance.isValidSafeHTML("test1", "<b>Jeff</b>", 100, false, errors));
+        assertTrue(instance.isValidSafeHTML("test2", "<a href=\"http://www.aspectsecurity.com\">Aspect Security</a>", 100, false, errors));
+        assertTrue(instance.isValidSafeHTML("test3", "Test. <script>alert(document.cookie)</script>", 100, false, errors));
+        assertTrue(instance.isValidSafeHTML("test4", "Test. <div style={xss:expression(xss)}>", 100, false, errors));
+        assertTrue(instance.isValidSafeHTML("test5", "Test. <s%00cript>alert(document.cookie)</script>", 100, false, errors));
+        assertTrue(instance.isValidSafeHTML("test6", "Test. <s\tcript>alert(document.cookie)</script>", 100, false, errors));
+        assertTrue(instance.isValidSafeHTML("test7", "Test. <s\r\n\0cript>alert(document.cookie)</script>", 100, false, errors));
+        assertTrue(errors.size() == 0);
+
+    }
+}
