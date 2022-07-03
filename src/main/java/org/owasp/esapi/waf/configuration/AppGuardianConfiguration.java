e4fc652ae20fe7ed69d0d18fc59846f0af067153
jeremiahjstacey <jeremiah.j.stacey@gmail.com>
2022-07-02 20:43:11 -0500

    Log4J Removal (#714)
    Close issue #534 - This is a planned removal of all Log4J 1 dependencies after nearly 2 years of deprecation. The release for this will be released on or shortly after 2022-07-13.
    
    * Suppression cleanup
    
    Clearing log4j1 suppressions from baseline.  Should cause maven site
    goal to fail if cleanup is not incomplete.
    
    * Log4J 1.x Source & Dependency Removal
    
    Removing baseline code & tests pertaining to log4j 1.x support.
    
    Removing log4j 1.x dependency from pom.
    
    * WAF Cleanup - Removing customized Log handling
    
    AppGuardianConfiguration had a declaration of a log4j log Level class
    which appears to never be used.
    
    ESAPIWebApplicationFirewallFilter attempts to compliment/override a
    pre-existing log4j1.x configuratino through the filterConfig with an
    expectation that ESAPI is configured for that Logger explicitly.
       * This has just been removed to allow the implementation to use the
         ESAPI Logger as configured.
    
    * Removing ant-javadoc.xml artifact
    
    Javadocs are generated in the maven build through the javadoc maven
    plugin.
    
    * log4j 1.x configuration file removal
    
    Remvoing log4j.dtd and log4j.xml instances from baseline.
    
    * log4j 1.x - Removing references in properties
    
    Cleaning up source and test references to the removed content.
    
    * log4j 1.x cleanup - scripts
    
    Removing log4j class contexts from the release process scripts.
    
    * JAVADOC CLEANUP
    
    Addressing warnings in javadoc generation.
      * Case sensitivity addressed in SecurityConfiguration.
      * Removed invalid class reference from Logger. This would have been an
        update; however, the reference point no longer fufills the reason
        for the initial link.
    
    This commit is superfluous to the log4j1.x removal intention of the
    branch where it was committed.
    
    * Merging suppressions file
    
    * Version updates
    
    Applying the latest plugin and dependency versions.
    
    * Comment Grammer cleanup.
    
    PR feedback.

--- src/main/java/org/owasp/esapi/waf/configuration/AppGuardianConfiguration.java
+++ src/main/java/org/owasp/esapi/waf/configuration/AppGuardianConfiguration.java
@@ -16,10 +16,8 @@
 package org.owasp.esapi.waf.configuration;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 
-import org.apache.log4j.Level;
 import org.owasp.esapi.waf.rules.Rule;
 
 /**
@@ -46,32 +44,6 @@ public class AppGuardianConfiguration {
     public static final int OPERATOR_IN_LIST = 2;
     public static final int OPERATOR_EXISTS = 3;
 
-    //// TODO - Delete these comments and the next 2 declarations on log4j clean-up.
-    /*
-     * We have static copies of the log settings so that the Rule objects
-     * can access them, because they don't have access to the instance of
-     * the configuration object.
-     */
-    /**
-     * @deprecated This {@code LOG_LEVEL} has never actually been used
-     * internally and this will be deleted when we remove all Log4J 1.x
-     * references.
-     */
-    @Deprecated
-    public static Level LOG_LEVEL = Level.INFO;    
-    /**
-     * @deprecated This {@code LOG_DIRECTORY} has never actually been used
-     * internally and this will be deleted when we remove all Log4J 1.x
-     * references.
-     */
-    @Deprecated
-    public static String LOG_DIRECTORY = "/WEB-INF/logs";
-
-    /*
-     * Logging settings.
-     */
-    private Level logLevel = Level.INFO;
-    private String logDirectory = "/WEB-INF/logs";
 
     /*
      * Default settings.
