1a1723a7139920d0160ccf116c4cd265e92e64f4
Dario Viva <darioviva@gmail.com>
2024-08-07 00:57:45 +0200

    AddSecureFlagRule: use for loop

--- src/main/java/org/owasp/esapi/waf/rules/AddSecureFlagRule.java
+++ src/main/java/org/owasp/esapi/waf/rules/AddSecureFlagRule.java
@@ -50,9 +50,8 @@ public class AddSecureFlagRule extends Rule {
 
     public boolean doesCookieMatch(String cookieName) {
 
-        for(int i=0;i<name.size();i++) {
-            Pattern p = name.get(i);
-            if ( p.matcher(cookieName).matches() ) {
+        for (Pattern p : name) {
+            if (p.matcher(cookieName).matches()) {
                 return true;
             }
         }
