3af86fa6650dfe88803874219808c764a546191a
Dario Viva <darioviva@gmail.com>
2024-08-07 00:57:18 +0200

    AddHTTPOnlyFlagRule: use for loop

--- src/main/java/org/owasp/esapi/waf/rules/AddHTTPOnlyFlagRule.java
+++ src/main/java/org/owasp/esapi/waf/rules/AddHTTPOnlyFlagRule.java
@@ -50,9 +50,8 @@ public class AddHTTPOnlyFlagRule extends Rule {
 
     public boolean doesCookieMatch(String cookieName) {
 
-        for(int i=0;i<name.size();i++) {
-            Pattern p = name.get(i);
-            if ( p.matcher(cookieName).matches() ) {
+        for (Pattern p : name) {
+            if (p.matcher(cookieName).matches()) {
                 return true;
             }
         }
