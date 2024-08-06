b692d8332c8c37ae7a61581c40d4eea533b753a7
Dario Viva <darioviva@gmail.com>
2024-08-06 22:24:32 +0200

    EncodeForCSSTag: fix typo

--- src/main/java/org/owasp/esapi/tags/EncodeForCSSTag.java
+++ src/main/java/org/owasp/esapi/tags/EncodeForCSSTag.java
@@ -3,7 +3,7 @@ package org.owasp.esapi.tags;
 import org.owasp.esapi.Encoder;
 
 /**
- * JSP tag that encode's it's body for use in CSS.
+ * JSP tag that encodes its body for use in CSS.
  */
 public class EncodeForCSSTag extends BaseEncodeTag
 {
